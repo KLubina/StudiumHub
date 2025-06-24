223ef09dfe8ddacc8533f09924f54e303f18b97b
Kristian Lubina <kristian.lubina@outlook.com>
2025-06-24 13:40:25 +0200

    add it-bsc from hssh

---
+++ configs/it/base-config.js
@@ -0,0 +1,49 @@
+/* ==== IT BASE CONFIGURATION ==== */
+/* Grundlegende Konfiguration für den Information Technology B.Sc. Studiengang */
+
+window.StudiengangBaseConfig = {
+    // Grundlegende Informationen
+    title: "BSc Information Technology",
+    subtitle: "mind. 180 ECTS insgesamt",
+    legendTitle: "Farben-Legende",
+    creditUnit: "ECTS",
+    
+    // Layout-Konfiguration
+    layout: "years",
+    moduleSizing: "proportional",
+    basisArea: 2200,
+    defaultAspectRatio: 1.4,
+    
+    // Features
+    enableTooltips: false,
+    enableHover: true,
+    
+    // Aspekt-Verhältnisse für verschiedene Module
+    aspectRatios: {
+        "longName": 2.0,
+        "Transfer Project": 1.8,
+        "Action Learning and Action Research": 2.5,
+        "Bachelorarbeit": 3.0
+    },
+    
+    // Kategorien und ihre CSS-Klassen
+    kategorien: [
+        { name: "Grundlagen & Cornerstone", klasse: "grundlagen" },
+        { name: "Programmierung & Software Engineering", klasse: "programmierung" },
+        { name: "Kommunikation & Persönlichkeitsentwicklung", klasse: "kommunikation" },
+        { name: "Transfer Projects", klasse: "transfer" },
+        { name: "Spezialisierung & Schwerpunkt", klasse: "spezialisierung" },
+        { name: "Wahlpflichtfächer", klasse: "wahlpflicht" },
+        { name: "Abschluss & Forschung", klasse: "abschluss" }
+    ],
+    
+    kategorieZuKlasse: {
+        "grundlagen": "grundlagen",
+        "programmierung": "programmierung", 
+        "kommunikation": "kommunikation",
+        "transfer": "transfer",
+        "spezialisierung": "spezialisierung",
+        "wahlpflicht": "wahlpflicht",
+        "abschluss": "abschluss"
+    }
+};
