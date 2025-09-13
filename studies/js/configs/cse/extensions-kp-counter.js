83033d33348781ecf662aec5489fd7e9baeb3439
Kristian Lubina <kristian.lubina@outlook.com>
2025-09-13 18:18:01 +0200

    Remove unused CSE config and extension scripts

--- studies/js/configs/cse/extensions-kp-counter.js
+++
@@ -1,6 +0,0 @@
-// Stub KP counter for CSE - no-op placeholder
-(function(){
-  function CSEKPCounter(studienplan){ this.studienplan = studienplan; }
-  CSEKPCounter.prototype.init = function(){};
-  window.CSEKPCounter = window.CSEKPCounter || CSEKPCounter;
-})();
