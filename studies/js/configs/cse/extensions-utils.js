0ded6504926695fa5be56d973c8c357f2d03c13b
Kristian Lubina <kristian.lubina@outlook.com>
2025-09-13 18:17:07 +0200

    Remove CSE tooltip manager and utils scripts

--- studies/js/configs/cse/extensions-utils.js
+++
@@ -1,9 +0,0 @@
-/* ==== CSE EXTENSIONS UTILITIES ==== */
-/* Hilfsfunktionen für CSE-spezifische Erweiterungen */
-
-window.CSEExtensionsUtils = {
-  // Utility-Funktionen für CSE könnten hier definiert werden
-  // Aktuell nicht benötigt, da Funktionalitäten in separaten Klassen organisiert sind
-};
-
-console.log('ℹ️ CSE extensions-utils.js geladen');
