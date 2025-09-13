29f9489b14bf9e4a2d8589ec5733fec228f05f0a
Kristian Lubina <kristian.lubina@outlook.com>
2025-09-13 18:55:51 +0200

    Simplify CSE exam blocks by removing module details

--- studies/js/configs/cse/base-config.js
+++ studies/js/configs/cse/base-config.js
@@ -38,59 +38,36 @@ window.StudiengangBaseConfig = {
     // Bereiche-Reihenfolge für 3. Jahr
     bereicheReihenfolge: ["Kernfächer", "Vertiefungsgebiet", "Wahlfächer", "Abschluss"],
     
-    // Prüfungsblöcke (spezifisch für CSE)
+    // Prüfungsblöcke (vereinfacht für CSE - ohne Moduldetails)
     pruefungsbloecke: [
         {
             name: "Basisprüfungsblock 1",
-            module: [
-                { name: "Lineare Algebra", ng: 1 },
-                { name: "Informatik", ng: 1 },
-                { name: "Diskrete Mathematik", ng: 1 }
-            ],
+            module: [],
             cssClass: "basis1"
         },
         {
             name: "Basisprüfungsblock 2",
-            module: [
-                { name: "Analysis I und II", ng: 3 },
-                { name: "Physik I und II", ng: 2 },
-                { name: "Mathematische Methoden (ehem. Komplexe Analysis)", ng: 1 },
-                { name: "Datenstrukturen und Algorithmen", ng: 2 },
-                { name: "Chemie", ng: 1 }
-            ],
+            module: [],
             cssClass: "basis2"
         },
         {
             name: "Prüfungsblock G1",
-            module: [
-                { name: "Analysis III", ng: 1 },
-                { name: "Introduction to Mathematical Optimization", ng: 1 },
-                { name: "Numerische Methoden für CSE", ng: 2 }
-            ],
+            module: [],
             cssClass: "block-g1"
         },
         {
             name: "Prüfungsblock G2", 
-            module: [
-                { name: "Programmiertechniken für physikalische Simulationen", ng: 1 },
-                { name: "Systems Programming and Computer Architecture", ng: 1 }
-            ],
+            module: [],
             cssClass: "block-g2"
         },
         {
             name: "Prüfungsblock G3",
-            module: [
-                { name: "Numerical Methods for Partial Differential Equations", ng: 2 },
-                { name: "Wahrscheinlichkeitstheorie und Statistik (ehem. Stochastik)", ng: 1 }
-            ],
+            module: [],
             cssClass: "block-g3"
         },
         {
             name: "Prüfungsblock G4",
-            module: [
-                { name: "Fluid Dynamics I", ng: 1 },
-                { name: "Statistische Physik und Computer Simulation", ng: 1 }
-            ],
+            module: [],
             cssClass: "block-g4"
         }
     ],
