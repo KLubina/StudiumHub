bc4f1f5ce4143c9f5bc63bfb470c152c6c3b0ea1
Kristian Lubina <kristian.lubina@outlook.com>
2025-11-06 15:48:22 +0100

    Add HTML subtitle with semester links to base config

--- studies/js/mono/configs/sbg/base-config.js
+++ studies/js/mono/configs/sbg/base-config.js
@@ -5,6 +5,10 @@ window.StudiengangBaseConfig = {
     // === 1. BASIC INFO ===
     title: "BSc Sport, Bewegung und Gesundheit",
     legendTitle: "Farben-Legende",
+        // Optional HTML subtitle (renders clickable links when provided)
+    subtitleHtml: `Herbstsemester: <a href="https://vorlesungsverzeichnis.unibas.ch/de/vorlesungsverzeichnis?periode=2025004&keyword=&hnode=169725&hid=&search=1#searchResults" target="_blank" rel="noopener">VVZ Herbst 2025</a>
+        <br>
+        Sommersemester: <a href="https://vorlesungsverzeichnis.unibas.ch/de/vorlesungsverzeichnis?periode=2024005&keyword=&hnode=169725&hid=&search=1#searchResults" target="_blank" rel="noopener">VVZ Sommer 2025</a>`,
     creditUnit: "ECTS",
 
     // === 2. LAYOUT CONFIG ===
