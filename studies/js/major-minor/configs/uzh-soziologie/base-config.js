c239a3739c1171899b24e39a00a8f0b1248848bb
Kristian Lubina <kristian.lubina@outlook.com>
2025-11-08 21:12:54 +0100

    Add subtitleHtml with VVZ and Studieninfo links

--- studies/js/major-minor/configs/uzh-soziologie/base-config.js
+++ studies/js/major-minor/configs/uzh-soziologie/base-config.js
@@ -6,6 +6,13 @@ window.StudiengangBaseConfig = {
     title: "Bachelor of Arts - Soziologie (Major)",
     legendTitle: "Farben-Legende",
     creditUnit: "ECTS",
+
+    // Optional HTML subtitle (renders clickable links when provided)
+    subtitleHtml: `Herbstsemester: <a href="https://studentservices.uzh.ch/uzh/anonym/vvz/index.html?sap-language=DE&sap-ui-language=DE#/details/2025/003/CGStudyProgramDetail/50893134/50000007/Philosophische%2520Fakult%25C3%25A4t/50890576/Bachelor%2520of%2520Arts%2520in%2520Sozialwissenschaften%2520(RVO19)/true/50893134/Soziologie/2025/003" target="_blank" rel="noopener">VVZ Herbst 2025</a>
+        <br>
+        Sommersemester: <a href="https://studentservices.uzh.ch/uzh/anonym/vvz/index.html?sap-language=DE&sap-ui-language=DE#/details/2024/004/CGStudyProgramDetail/50893134/50000007/Philosophische%2520Fakult%25C3%25A4t/50890576/Bachelor%2520of%2520Arts%2520in%2520Sozialwissenschaften%2520(RVO19)/true/50893134/Soziologie/2024/004" target="_blank" rel="noopener">VVZ Sommer 2024</a>
+        <br>
+        Studieninfo: <a href="https://www.suz.uzh.ch/de/bachelor.html" target="_blank" rel="noopener">Studium Bachelor</a>`,
     
     // === 2. LAYOUT CONFIG ===
     layout: "years",
