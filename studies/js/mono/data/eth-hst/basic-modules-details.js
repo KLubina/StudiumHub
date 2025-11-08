550ac69c1bef69323808b74207b7871bbddd738b
Kristian Lubina <kristian.lubina@outlook.com>
2025-11-08 13:12:03 +0100

    Add ETH HST basic modules details data file

---
+++ studies/js/mono/data/eth-hst/basic-modules-details.js
@@ -0,0 +1,106 @@
+window.StudiengangModuleDetails = {
+  // #region 1. Semester - Basisprüfungsblock 1
+  "Molekulare Genetik und Zellbiologie": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=194171&semkez=2025W&ansicht=LEHRVERANSTALTUNGEN&lang=de",
+  },
+  "Allgemeine Chemie": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=193363&semkez=2025W&ansicht=LEHRVERANSTALTUNGEN&lang=de",
+  },
+  "Grundlagen der Informatik": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=193758&semkez=2025W&ansicht=LEHRVERANSTALTUNGEN&lang=de",
+  },
+  "Einführung Gesundheitswissenschaften und Technologie I": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=193757&semkez=2025W&ansicht=LEHRVERANSTALTUNGEN&lang=de",
+  },
+  // #endregion
+
+  // #region 1. Semester - Basisprüfungsblock 2
+  "Mathematik I": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=192849&semkez=2025W&ansicht=LEHRVERANSTALTUNGEN&lang=de",
+  },
+  "Organische Chemie I": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=193393&semkez=2025W&ansicht=LEHRVERANSTALTUNGEN&lang=de",
+  },
+  // #endregion
+
+  // #region 2. Semester - Basisprüfungsblock 2
+  "Infektion": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=188594&semkez=2025S&ansicht=LEHRVERANSTALTUNGEN&lang=de",
+  },
+  "Biochemie": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=188655&semkez=2025S&ansicht=LEHRVERANSTALTUNGEN&lang=de",
+  },
+  "Organische Chemie II": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=188324&semkez=2025S&ansicht=LEHRVERANSTALTUNGEN&lang=de",
+  },
+  "Biomechanik I": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=188267&semkez=2025S&ansicht=LEHRVERANSTALTUNGEN&lang=de",
+  },
+  "Mathematik II": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=187283&semkez=2025S&ansicht=LEHRVERANSTALTUNGEN&lang=de",
+  },
+  "Statistik I": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=188479&semkez=2025S&ansicht=LEHRVERANSTALTUNGEN&lang=de",
+  },
+  "Praktikum Chemie": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=187326&semkez=2025S&ansicht=LEHRVERANSTALTUNGEN&lang=de",
+  },
+  // #endregion
+
+  // #region 3. Semester - Kernfächer Block A
+  "Humanphysiologie I": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=194078&semkez=2025W&ansicht=LEHRVERANSTALTUNGEN&lang=de",
+  },
+  "Mathematik III": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=192855&semkez=2025W&ansicht=LEHRVERANSTALTUNGEN&lang=de",
+  },
+  "Statistik II": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=193301&semkez=2025W&ansicht=LEHRVERANSTALTUNGEN&lang=de",
+  },
+  // #endregion
+
+  // #region 3. Semester - Kernfächer Block B
+  "Physik I": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=194153&semkez=2025W&ansicht=LEHRVERANSTALTUNGEN&lang=de",
+  },
+  // #endregion
+
+  // #region 3. Semester - Einzelfächer und Praktika
+  "Laboratory Course in Medical Technology": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=194725&semkez=2025W&ansicht=LEHRVERANSTALTUNGEN&lang=de",
+  },
+  "Produktentwicklung in der Medizintechnik": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=194204&semkez=2025W&ansicht=LEHRVERANSTALTUNGEN&lang=de",
+  },
+  // #endregion
+
+  // #region 4. Semester - Kernfächer Block B
+  "Molekular- und Zellbiologie in Gesundheit und Krankheit": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=187208&semkez=2025S&ansicht=LEHRVERANSTALTUNGEN&lang=de",
+  },
+  "Histologie": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=187873&semkez=2025S&ansicht=LEHRVERANSTALTUNGEN&lang=de",
+  },
+  "Physik II": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=188034&semkez=2025S&ansicht=LEHRVERANSTALTUNGEN&lang=de",
+  },
+  // #endregion
+
+  // #region 4. Semester - Kernfächer Block C
+  "Humanphysiologie II": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=187706&semkez=2025S&ansicht=LEHRVERANSTALTUNGEN&lang=de",
+  },
+  "Biomechanik II": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=187724&semkez=2025S&ansicht=LEHRVERANSTALTUNGEN&lang=de",
+  },
+  "Biomedizinische Grenzflächen": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=188502&semkez=2025S&ansicht=LEHRVERANSTALTUNGEN&lang=de",
+  },
+  // #endregion
+
+  // #region 4. Semester - Einzelfächer und Praktika
+  "Praktikum Physiologie": {
+    link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=187457&semkez=2025S&ansicht=LEHRVERANSTALTUNGEN&lang=de",
+  },
+  // #endregion
+};
