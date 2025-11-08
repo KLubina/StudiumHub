423be0db6ea1661b3a1e5f97fe77a03a02e8dbef
Kristian Lubina <kristian.lubina@outlook.com>
2025-11-08 19:15:10 +0100

    Refactor UZH Geschichte module data to JS files

---
+++ studies/js/major-minor/data/uzh-geschichte/pflichtmodule-data.js
@@ -0,0 +1,104 @@
+/* ==== GESCHICHTE PFLICHTMODULE DATA ==== */
+// Pflichtmodule für Geschichte (HS 2025 + FS 2026)
+
+window.GeschichtePflichtmoduleData = {
+  // Frühjahrssemester 2026 – Geschichte Pflichtmodule
+  fs2026: {
+    pflichtmodule: [
+      {
+        name: "Bachelorarbeit",
+        kp: 15,
+        kategorie: "Pflichtmodule",
+        semester: "FS",
+        jahr: 3,
+      },
+      {
+        name: "Basismodul I",
+        kp: 9,
+        kategorie: "Pflichtmodule",
+        semester: "FS",
+      },
+      {
+        name: "Basismodul II",
+        kp: 9,
+        kategorie: "Pflichtmodule",
+        semester: "FS",
+      },
+      {
+        name: "Einführungsvorlesung Frühling",
+        kp: 3,
+        kategorie: "Pflichtmodule",
+        semester: "FS",
+      },
+      {
+        name: "Grundlagen Latein",
+        kp: 6,
+        kategorie: "Pflichtmodule",
+        semester: "FS",
+      },
+      {
+        name: "Methoden und Theorie",
+        kp: 9,
+        kategorie: "Pflichtmodule",
+        semester: "FS",
+      },
+      {
+        name: "Mündliche Prüfung",
+        kp: 9,
+        kategorie: "Pflichtmodule",
+        semester: "FS",
+        jahr: 3,
+      },
+    ],
+  },
+
+  // Herbstsemester 2025 – Geschichte Pflichtmodule
+  hs2025: {
+    pflichtmodule: [
+      {
+        name: "Bachelorarbeit",
+        kp: 15,
+        kategorie: "Pflichtmodule",
+        semester: "HS",
+        jahr: 3,
+      },
+      {
+        name: "Basismodul I",
+        kp: 9,
+        kategorie: "Pflichtmodule",
+        semester: "HS",
+      },
+      {
+        name: "Basismodul II",
+        kp: 9,
+        kategorie: "Pflichtmodule",
+        semester: "HS",
+      },
+      {
+        name: "Einführungsvorlesung Herbst",
+        kp: 3,
+        kategorie: "Pflichtmodule",
+        semester: "HS",
+      },
+      {
+        name: "Grundlagen Latein",
+        kp: 6,
+        kategorie: "Pflichtmodule",
+        semester: "HS",
+      },
+      {
+        name: "Methoden und Theorie",
+        kp: 9,
+        kategorie: "Pflichtmodule",
+        semester: "HS",
+      },
+      {
+        name: "Mündliche Prüfung",
+        kp: 9,
+        kategorie: "Pflichtmodule",
+        semester: "HS",
+        jahr: 3,
+      },
+    ],
+  },
+};
