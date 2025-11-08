50c39f36737422d545849ec26cc9694b4c539fd7
Kristian Lubina <kristian.lubina@outlook.com>
2025-11-08 13:26:06 +0100

    Add course descriptions to module details

--- studies/js/mono/data/eth-hst/basic-modules-details.js
+++ studies/js/mono/data/eth-hst/basic-modules-details.js
@@ -1,105 +1,130 @@
 window.StudiengangModuleDetails = {
   // #region 1. Semester - Basisprüfungsblock 1
   "Molekulare Genetik und Zellbiologie": {
+    kurzbeschreibung: "Dieser Kurs vermittelt die Grundprinzipien der Evolution, Zellbiologie, Molekularbiologie, Genetik und Entwicklungsbiologie am Beispiel Mensch.",
     link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=194171&semkez=2025W&ansicht=LEHRVERANSTALTUNGEN&lang=de",
   },
   "Allgemeine Chemie": {
+    kurzbeschreibung: "Die Vorlesung erläutert einige Grundlagen der allgemeinen Chemie. Dies schliesst (unter anderen) Reaktionsgleichungen, Energieumsatz bei chemischen Reaktionen, Eigenschaften von ionische und kovalente Bindungen, Lewisschen Strukturen, Eigenschaften von Lösungen, Kinetik, Thermodynamik, Säure-Basen Gleichgewichte, Elektrochemie und Eigenschaften von Metalle Komplexe ein.",
     link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=193363&semkez=2025W&ansicht=LEHRVERANSTALTUNGEN&lang=de",
   },
   "Grundlagen der Informatik": {
+    kurzbeschreibung: "Diese Lehrveranstaltung vermittelt ausgewählte Informatik-Konzepte zur Bearbeitung interdisziplinärer Projekte. Themenbereiche: Einführung in die Programmierung, Sequenzanalyse, Simulieren und Modellieren, Matrizenrechnen, Daten verwalten mit relationalen Datenbanken.",
     link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=193758&semkez=2025W&ansicht=LEHRVERANSTALTUNGEN&lang=de",
   },
   "Einführung Gesundheitswissenschaften und Technologie I": {
+    kurzbeschreibung: "Übersicht über verschiedene Aspekte von Gesundheit und Krankheit (Gesundheitsmodelle, Diagnostik und Therapie von Krankheiten, Prävention, Epidemiologie); Einführung in technologische Aspekte (Mechanik, Messtechnik, Regelung); Grundlagen des wissenschaftlichen Arbeitens (Ethik, Literaturrecherche, Studiendesign, Datenerfassung, Datenauswertung und Datendarstellung).",
     link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=193757&semkez=2025W&ansicht=LEHRVERANSTALTUNGEN&lang=de",
   },
   // #endregion
 
   // #region 1. Semester - Basisprüfungsblock 2
   "Mathematik I": {
+    kurzbeschreibung: "Mathematik I/II ist eine Einführung in die ein- und mehrdimensionale Analysis und die Lineare Algebra unter besonderer Betonung von Anwendungen in den Naturwissenschaften.",
     link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=192849&semkez=2025W&ansicht=LEHRVERANSTALTUNGEN&lang=de",
   },
   "Organische Chemie I": {
+    kurzbeschreibung: "Grundlagen der Organischen Chemie: Strukturlehre. Bindungsverhältnisse und funktionelle Gruppen; Nomenklatur; Resonanz und Aromatizität; Stereochemie; Konformationsanalyse; Bindungsstärken; organische Säuren und Basen; Einführung in die Reaktionslehre; reaktive Zwischenstufen: Carbanionen, Carbeniumionen und Radikale.",
     link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=193393&semkez=2025W&ansicht=LEHRVERANSTALTUNGEN&lang=de",
   },
   // #endregion
 
   // #region 2. Semester - Basisprüfungsblock 2
   "Infektion": {
+    kurzbeschreibung: "Aufbau und Funktion von pathogenen Bakterien, Viren, Pilzen, sowie ein- und mehrzelliger Parasiten. Die Vorlesung wird von einer Lernplattform begleitet. Einzelne Inhalte müssen im Selbststudium erarbeitet werden.",
     link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=188594&semkez=2025S&ansicht=LEHRVERANSTALTUNGEN&lang=de",
   },
   "Biochemie": {
+    kurzbeschreibung: "Der Kurs vermittelt Studierenden die zentralen Fakten und Konzepte der Biochemie und behandelt Themen aus den Bereichen Struktur, physico-chemischen Eigenschaften und Funktion von Biomolekülen; Enzyme und deren Funktionsweise; menschlicher Stoffwechsel und dessen Regulation; Signaltransduktion und Motorproteine.",
     link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=188655&semkez=2025S&ansicht=LEHRVERANSTALTUNGEN&lang=de",
   },
   "Organische Chemie II": {
+    kurzbeschreibung: "Der zentrale Zusammenhang zwischen Struktur und Reaktivität organischer Moleküle wird anhand grundlegender Reaktionstypen der organischen Chemie aufgezeigt. Damit einhergehend wird ein elementares Syntheserepertoire erarbeitet.",
     link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=188324&semkez=2025S&ansicht=LEHRVERANSTALTUNGEN&lang=de",
   },
   "Biomechanik I": {
     link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=188267&semkez=2025S&ansicht=LEHRVERANSTALTUNGEN&lang=de",
   },
   "Mathematik II": {
+    kurzbeschreibung: "Mathematik I/II ist eine Einführung in die ein- und mehrdimensionale Analysis und die Lineare Algebra unter besonderer Betonung von Anwendungen in den Naturwissenschaften.",
     link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=187283&semkez=2025S&ansicht=LEHRVERANSTALTUNGEN&lang=de",
   },
   "Statistik I": {
+    kurzbeschreibung: "Einführung in einfache Methoden und grundlegende Begriffe von Statistik und Wahrscheinlichkeitsrechnung für Nichtmathematiker. Die Konzepte werden anhand einiger anschaulicher Beispiele eingeführt.",
     link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=188479&semkez=2025S&ansicht=LEHRVERANSTALTUNGEN&lang=de",
   },
   "Praktikum Chemie": {
+    kurzbeschreibung: "Im Praktikum Chemie werden grundlegende Techniken der Laborarbeit erlernt. Die Experimente umfassen sowohl analytische als auch präparative Aufgaben. So werden z. B. Bodenproben analysiert, ausgewählte Synthesen durchgeführt, und die Arbeit mit gasförmigen Substanzen im Labor wird vermittelt.",
     link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=187326&semkez=2025S&ansicht=LEHRVERANSTALTUNGEN&lang=de",
   },
   // #endregion
 
   // #region 3. Semester - Kernfächer Block A
   "Humanphysiologie I": {
+    kurzbeschreibung: "In diesem Kurs werden die physiologischen Prozesse des menschlichen Körpers behandelt.",
     link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=194078&semkez=2025W&ansicht=LEHRVERANSTALTUNGEN&lang=de",
   },
   "Mathematik III": {
+    kurzbeschreibung: "Vertiefung der mehrdimensionalen Analysis mit Schwerpunkt in der Anwendung der partiellen Differentialgleichungen, Vertiefung der Linearen Algebra und Einführung in die Systemanalyse und Modellbildung.",
     link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=192855&semkez=2025W&ansicht=LEHRVERANSTALTUNGEN&lang=de",
   },
   "Statistik II": {
+    kurzbeschreibung: "Vertiefung von Statistikmethoden. Nach dem detailierten Fundament aus Statistik I liegt nun der Fokus auf konzeptueller Breite und konkreter Problemlösungsfähigkeit mit der Statistiksoftware R.",
     link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=193301&semkez=2025W&ansicht=LEHRVERANSTALTUNGEN&lang=de",
   },
   // #endregion
 
   // #region 3. Semester - Kernfächer Block B
   "Physik I": {
+    kurzbeschreibung: "Die Vorlesung bietet eine Einführung in die klassische Physik, mit speziellen Fokus auf Anwendungen in der Medizin.",
     link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=194153&semkez=2025W&ansicht=LEHRVERANSTALTUNGEN&lang=de",
   },
   // #endregion
 
   // #region 3. Semester - Einzelfächer und Praktika
   "Laboratory Course in Medical Technology": {
+    kurzbeschreibung: "Dieses Praktikum dient dazu angewendete Grundlagen im CAD, FEM, Produktoptimierung, mechanischer Belastungstest, Softwareentwicklung und Hardwareeinsatz in der Robotik zu vermitteln.",
     link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=194725&semkez=2025W&ansicht=LEHRVERANSTALTUNGEN&lang=de",
   },
   "Produktentwicklung in der Medizintechnik": {
+    kurzbeschreibung: "Der Kurs bietet einen Einblick in verschiedene Aspekte der Entwicklung von Medizintechnik-Produkten wie Anforderungsanalyse, Forschung und Entwicklung, Validierung, Zulassung und klinische Evaluation.",
     link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=194204&semkez=2025W&ansicht=LEHRVERANSTALTUNGEN&lang=de",
   },
   // #endregion
 
   // #region 4. Semester - Kernfächer Block B
   "Molekular- und Zellbiologie in Gesundheit und Krankheit": {
+    kurzbeschreibung: "Der Kurs konzentriert sich auf die biochemischen, molekularen und zellulären Mechanismen der Krankheitsentwicklung. Es werden verschiedene Bereiche abgedeckt, die auf den Kursen in Molekularer Genetik und Zellbiologie sowie Biochemie aufbauen, um ein konzeptionelles Verständnis dieser Prozesse zu entwickeln.",
     link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=187208&semkez=2025S&ansicht=LEHRVERANSTALTUNGEN&lang=de",
   },
   "Histologie": {
+    kurzbeschreibung: "Auf den Vorlesungen Anatomie 1+2 aufbauendes Online-Praktikum am vituellen Mikroskop Biolucida mit einer Einführung in histologische Technik. Im ersten Teil werden Beispiele von Epithelgewebe, Binde- und Stützgewebe, Muskelgewebe und Nervengewebe untersucht. Der zweite Teil behandelt die mikroskopische Anatomie ausgewählter Organe.",
     link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=187873&semkez=2025S&ansicht=LEHRVERANSTALTUNGEN&lang=de",
   },
   "Physik II": {
+    kurzbeschreibung: "Diese Vorlesung bietet eine Einführung in die klassische Physik mit besonderem Schwerpunkt auf Anwendungen in der Medizin. Sie vermittelt Schlüsselkonzepte in Strömungslehre, Thermodynamik, Elektromagnetismus und Optik. Besonders viel Wert wird darauf gelegt, wichtige Phänomene mithilfe von Experimenten zu veranschaulichen, sowie Fähigkeiten zur quantitativen Problemlösung zu entwickeln.",
     link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=188034&semkez=2025S&ansicht=LEHRVERANSTALTUNGEN&lang=de",
   },
   // #endregion
 
   // #region 4. Semester - Kernfächer Block C
   "Humanphysiologie II": {
+    kurzbeschreibung: "Dieser Kurs vermittelt die Grundlagen der Physiologie und Anatomie des menschlichen Körpers.",
     link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=187706&semkez=2025S&ansicht=LEHRVERANSTALTUNGEN&lang=de",
   },
   "Biomechanik II": {
+    kurzbeschreibung: "Einführung in die Dynamik, Kinetik und Kinematik von starren und elastischen Mehrkörpersystemen mit Anwendungen in Technologie, Biologie und Medizin und insbesondere der menschlichen Bewegung.",
     link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=187724&semkez=2025S&ansicht=LEHRVERANSTALTUNGEN&lang=de",
   },
   "Biomedizinische Grenzflächen": {
+    kurzbeschreibung: "Diese Vorlesung dient als Einführung in das Design von Materialien für Anwendungen in der Biomedizin. Der Fokus liegt auf der Kontrolle der Wechselwirkungen zwischen Biomolekülen, Bakterien oder Zellen mit synthetischen Materialien. Die Kenntnis grundlegender Konzepte erlaubt uns, Anforderungen an Materialoberflächen zu definieren und molekulare Prinzipien zu deren Realisierung zu nutzen.",
     link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=188502&semkez=2025S&ansicht=LEHRVERANSTALTUNGEN&lang=de",
   },
   // #endregion
 
   // #region 4. Semester - Einzelfächer und Praktika
   "Praktikum Physiologie": {
+    kurzbeschreibung: "Experimente zur Funktion von Nerven, Muskeln, Herz, Kreislauf, Atmung und Sinnesorganen beim Menschen.",
     link: "https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=187457&semkez=2025S&ansicht=LEHRVERANSTALTUNGEN&lang=de",
   },
   // #endregion
