3689b39d1e01e536e400ebb7e9c9191b6f99f5aa
Kristian Lubina <kristian.lubina@outlook.com>
2025-11-08 16:28:46 +0100

    Remove subtitleHtml from UZH major configs

--- studies/js/major-minor/configs/uzh-kommunikation/base-config.js
+++ studies/js/major-minor/configs/uzh-kommunikation/base-config.js
@@ -6,12 +6,6 @@ window.StudiengangBaseConfig = {
     title: "Bachelor of Arts - Kommunikationswissenschaft und Medienforschung (Major)",
     legendTitle: "Farben-Legende",
     creditUnit: "ECTS",
-    // Optional HTML subtitle (renders clickable links when provided)
-    subtitleHtml: `Herbstsemester: <a href="https://studentservices.uzh.ch/uzh/anonym/vvz/index.html?sap-language=DE&sap-ui-language=DE#/details/2025/003/CGStudyProgramDetail/50891466/50000007/Philosophische%2520Fakult%25C3%25A4t/50890575/Bachelor%2520of%2520Arts%2520(RVO19)/true/50891466/Kommunikationswissenschaft%2520und%2520Medienforschung/2025/003" target="_blank" rel="noopener">VVZ Herbst 2025</a>
-        <br>
-        Sommersemester: <a href="https://studentservices.uzh.ch/uzh/anonym/vvz/index.html?sap-language=DE&sap-ui-language=DE#/details/2024/004/CGStudyProgramDetail/50891466/50000007/Philosophische%2520Fakult%25C3%25A4t/50890575/Bachelor%2520of%2520Arts%2520(RVO19)/true/50891466/Kommunikationswissenschaft%2520und%2520Medienforschung/2024/004" target="_blank" rel="noopener">VVZ Sommer 2024</a>
-        <br>
-        Studieninfo: <a href="https://www.ikmz.uzh.ch/de/studies/ba.html" target="_blank" rel="noopener">Studium Bachelor</a> | <a href="https://www.phil.uzh.ch/static/rechtssammlung/bachelor/regulaer-nachfolger/mc/MC_06B-7840-120.pdf" target="_blank" rel="noopener">Studienplan & Reglement (PDF)</a>`,
 
     // === 2. LAYOUT CONFIG ===
     layout: "years",
--- studies/js/major-minor/configs/uzh-soziologie/base-config.js
+++ studies/js/major-minor/configs/uzh-soziologie/base-config.js
@@ -6,13 +6,7 @@ window.StudiengangBaseConfig = {
     title: "Bachelor of Arts - Soziologie (Major)",
     legendTitle: "Farben-Legende",
     creditUnit: "ECTS",
-    // Optional HTML subtitle (renders clickable links when provided)
-    subtitleHtml: `Herbstsemester: <a href="https://studentservices.uzh.ch/uzh/anonym/vvz/index.html?sap-language=DE&sap-ui-language=DE#/details/2025/003/CGStudyProgramDetail/50891623/50000007/Philosophische%2520Fakult%25C3%25A4t/50890575/Bachelor%2520of%2520Arts%2520(RVO19)/true/50891623/Soziologie/2025/003" target="_blank" rel="noopener">VVZ Herbst 2025</a>
-        <br>
-        Sommersemester: <a href="https://studentservices.uzh.ch/uzh/anonym/vvz/index.html?sap-language=DE&sap-ui-language=DE#/details/2024/004/CGStudyProgramDetail/50891623/50000007/Philosophische%2520Fakult%25C3%25A4t/50890575/Bachelor%2520of%2520Arts%2520(RVO19)/true/50891623/Soziologie/2024/004" target="_blank" rel="noopener">VVZ Sommer 2024</a>
-        <br>
-        Studieninfo: <a href="https://www.suz.uzh.ch/de/institut/studium/bachelor.html" target="_blank" rel="noopener">Studium Bachelor</a> | <a href="https://www.phil.uzh.ch/static/rechtssammlung/bachelor/regulaer-nachfolger/mc/MC_06B-8400-120.pdf" target="_blank" rel="noopener">Studienplan & Reglement (PDF)</a>`,
-
+    
     // === 2. LAYOUT CONFIG ===
     layout: "years",
     moduleSizing: "proportional",
