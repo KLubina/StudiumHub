49712ab25bad657b0060a59c1dee7d95c8ed17f8
Kristian Lubina <kristian.lubina@outlook.com>
2025-11-09 21:46:20 +0100

    Remove KP counter configs for multiple study programs

--- studies/js/mono/eth-itet/config/kp-counter-config.js
+++
@@ -1,7 +0,0 @@
-/* ==== KP COUNTER CONFIGURATION ==== */
-/* Settings for the KP counter feature */
-
-window.StudiengangKPCounterConfig = {
-    // Minimal: use default/simple KP counter without extras
-    kpCounterConfig: {}
-};
