9064c1989a7db45ed1e52c9d438a6a409d9e5f70
Kristian Lubina <kristian.lubina@outlook.com>
2025-11-10 22:54:50 +0100

    Remove 'ng' property from module data entries

--- study-visualization/program-specific/mono/eth-cse/data/basic-modules-data.js
+++ study-visualization/program-specific/mono/eth-cse/data/basic-modules-data.js
@@ -5,61 +5,59 @@ window.StudiengangModules = [
   // #region 1. Semester
   // HINWEIS: Farb-Zuordnungen (Themenbereiche & Prüfungsblöcke) befinden sich in color-config.js
 
-  { jahr: 1, semester: 1, name: "Physik I", kp: 4, ng: 2 },
-  { jahr: 1, semester: 1, name: "Informatik", kp: 4, ng: 1 },
-  { jahr: 1, semester: 1, name: "Lineare Algebra", kp: 5, ng: 1 },
-  { jahr: 1, semester: 1, name: "Diskrete Mathematik", kp: 7, ng: 1 },
-  { jahr: 1, semester: 1, name: "Analysis I", kp: 8, ng: 3 },
+  { jahr: 1, semester: 1, name: "Physik I", kp: 4 },
+  { jahr: 1, semester: 1, name: "Informatik", kp: 4 },
+  { jahr: 1, semester: 1, name: "Lineare Algebra", kp: 5 },
+  { jahr: 1, semester: 1, name: "Diskrete Mathematik", kp: 7 },
+  { jahr: 1, semester: 1, name: "Analysis I", kp: 8 },
 
   // #endregion 1. Semester
 
   // #region 2. Semester
-  { jahr: 1, semester: 2, name: "Physik II", kp: 4, ng: 2 },
+  { jahr: 1, semester: 2, name: "Physik II", kp: 4 },
   {
     jahr: 1,
     semester: 2,
     name: "Mathematische Methoden (ehem. Komplexe Analysis)",
     kp: 4,
-    ng: 1,
   },
-  { jahr: 1, semester: 2, name: "Chemie", kp: 4, ng: 1 },
-  { jahr: 1, semester: 2, name: "Analysis II", kp: 8, ng: 3 },
+  { jahr: 1, semester: 2, name: "Chemie", kp: 4 },
+  { jahr: 1, semester: 2, name: "Analysis II", kp: 8 },
   {
     jahr: 1,
     semester: 2,
     name: "Datenstrukturen und Algorithmen",
     kp: 8,
-    ng: 2,
   },
   // #endregion 2. Semester
 
   // #region 3.-4. Semester (ohne Semesterunterscheidung)
-  { jahr: 2, name: "Analysis III", kp: 4, ng: 1 },
-  { jahr: 2, name: "Introduction to Mathematical Optimization", kp: 5, ng: 1 },
-  { jahr: 2, name: "Numerische Methoden für CSE", kp: 9, ng: 2 },
+  { jahr: 2, name: "Analysis III", kp: 4 },
+  {
+    jahr: 2,
+    name: "Introduction to Mathematical Optimization",
+    kp: 5,
+  },
+  { jahr: 2, name: "Numerische Methoden für CSE", kp: 9 },
   {
     jahr: 2,
     name: "Wahrscheinlichkeitstheorie und Statistik (ehem. Stochastik)",
     kp: 4,
-    ng: 1,
   },
   {
     jahr: 2,
     name: "Numerical Methods for Partial Differential Equations",
     kp: 10,
-    ng: 2,
   },
   {
     jahr: 2,
     name: "Programmiertechniken für physikalische Simulationen",
     kp: 5,
-    ng: 1,
   },
   {
     jahr: 2,
     name: "Systems Programming and Computer Architecture",
     kp: 7,
-    ng: 1,
   },
   {
     jahr: 2,
@@ -67,12 +65,11 @@ window.StudiengangModules = [
     kp: 6,
     kategorie: "wissenschaftliche-arbeit",
   },
-  { jahr: 2, name: "Fluid Dynamics I", kp: 8, ng: 1 },
+  { jahr: 2, name: "Fluid Dynamics I", kp: 8 },
   {
     jahr: 2,
     name: "Statistische Physik und Computer Simulation",
     kp: 5,
-    ng: 1,
   },
   // #endregion 3.-4. Semester
 
