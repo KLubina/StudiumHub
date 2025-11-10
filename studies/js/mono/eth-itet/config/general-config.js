6c533fabd898d1540ddb9b7681d00bf8d9c2c4aa
Kristian Lubina <kristian.lubina@outlook.com>
2025-11-10 12:19:09 +0100

    Update subtitleHtml to use unescaped HTML tags

--- studies/js/mono/eth-itet/config/general-config.js
+++ studies/js/mono/eth-itet/config/general-config.js
@@ -6,13 +6,13 @@ window.StudiengangGeneralConfig = {
     title: "BSc Elektrotechnik und Informationstechnologie",
     
     // Optional HTML subtitle (renders clickable links when provided)
-    subtitleHtml: `Herbstsemester: &lt;a href="https://www.vvz.ethz.ch/Vorlesungsverzeichnis/sucheLehrangebot.view?lerneinheitscode=&amp;deptId=18&amp;famname=&amp;unterbereichAbschnittId=&amp;seite=0&amp;lerneinheitstitel=&amp;rufname=&amp;kpRange=0,999&amp;lehrsprache=&amp;bereichAbschnittId=&amp;semkez=2025W&amp;studiengangAbschnittId=117363&amp;studiengangTyp=BSC&amp;ansicht=1&amp;lang=de&amp;katalogdaten=&amp;wahlinfo=" target="_blank" rel="noopener"&gt;VVZ Herbst 2025&lt;/a&gt;
-        &lt;br&gt;
-        Sommersemester: &lt;a href="https://www.vvz.ethz.ch/Vorlesungsverzeichnis/sucheLehrangebot.view?lerneinheitscode=&amp;deptId=18&amp;famname=&amp;unterbereichAbschnittId=&amp;seite=0&amp;lerneinheitstitel=&amp;rufname=&amp;kpRange=0,999&amp;lehrsprache=&amp;bereichAbschnittId=&amp;semkez=2025S&amp;studiengangAbschnittId=114361&amp;studiengangTyp=BSC&amp;ansicht=1&amp;lang=de&amp;katalogdaten=&amp;wahlinfo=" target="_blank" rel="noopener"&gt;VVZ Sommer 2025&lt;/a&gt;
-        &lt;br&gt;
-        &lt;a href="https://ethz.ch/de/studium/bachelor/studienangebot/ingenieurwissenschaften/elektrotechnik-und-informationstechnologie/details.html" target="_blank" rel="noopener"&gt;Studiengang-Seite&lt;/a&gt;
-        &lt;br&gt;
-        &lt;a href="https://rechtssammlung.sp.ethz.ch/Dokumente/Forms/311%20Bachelor%20BSc%20und%20BA.aspx#InplviewHash4dfcf9fd-a65f-462d-938a-df50fbd498ab=SortField%3DStand-SortDir%3DAsc" target="_blank" rel="noopener"&gt;Rechtssammlung BSc/BA&lt;/a&gt;`,
+    subtitleHtml: `Herbstsemester: <a href="https://www.vvz.ethz.ch/Vorlesungsverzeichnis/sucheLehrangebot.view?lerneinheitscode=&deptId=18&famname=&unterbereichAbschnittId=&seite=0&lerneinheitstitel=&rufname=&kpRange=0,999&lehrsprache=&bereichAbschnittId=&semkez=2025W&studiengangAbschnittId=117363&studiengangTyp=BSC&ansicht=1&lang=de&katalogdaten=&wahlinfo=" target="_blank" rel="noopener">VVZ Herbst 2025</a>
+        <br>
+        Sommersemester: <a href="https://www.vvz.ethz.ch/Vorlesungsverzeichnis/sucheLehrangebot.view?lerneinheitscode=&deptId=18&famname=&unterbereichAbschnittId=&seite=0&lerneinheitstitel=&rufname=&kpRange=0,999&lehrsprache=&bereichAbschnittId=&semkez=2025S&studiengangAbschnittId=114361&studiengangTyp=BSC&ansicht=1&lang=de&katalogdaten=&wahlinfo=" target="_blank" rel="noopener">VVZ Sommer 2025</a>
+        <br>
+        <a href="https://ethz.ch/de/studium/bachelor/studienangebot/ingenieurwissenschaften/elektrotechnik-und-informationstechnologie/details.html" target="_blank" rel="noopener">Studiengang-Seite</a>
+        <br>
+        <a href="https://rechtssammlung.sp.ethz.ch/Dokumente/Forms/311%20Bachelor%20BSc%20und%20BA.aspx#InplviewHash4dfcf9fd-a65f-462d-938a-df50fbd498ab=SortField%3DStand-SortDir%3DAsc" target="_blank" rel="noopener">Rechtssammlung: Bachelor BSc und BA</a>`,
 
     // Display settings
     legendTitle: "Farben-Legende",
