d5cbaf27072ee03ef1f37f78f2e1f6659db55a4a
Kristian Lubina <kristian.lubina@outlook.com>
2025-11-11 13:28:12 +0100

    Refactor Wahlmodule data to flat array structure

--- study-visualization/program-specific/mono/eth-rig/data/wahlmodule-data.js
+++ study-visualization/program-specific/mono/eth-rig/data/wahlmodule-data.js
@@ -4,157 +4,172 @@
   window.RIGModuleData bereit.
 */
 
-window.RIGModuleData = {
-  wahlmoduleBereiche: {
-    "Geodäsie und Satellitennavigation": [
-      {
-        // PFLICHTFELDER
-        name: "Physikalische und kinematische Geodäsie",
-        kp: 6,
-        kategorie: "wahlmodule",
-      },
-      {
-        // PFLICHTFELDER
-        name: "Globale Satellitennavigationssysteme",
-        kp: 3,
-        kategorie: "wahlmodule",
-      },
-      {
-        // PFLICHTFELDER
-        name: "Geodätische Datenanalyse",
-        kp: 3,
-        kategorie: "wahlmodule",
-      },
-      {
-        // PFLICHTFELDER
-        name: "Navigation",
-        kp: 3,
-        kategorie: "wahlmodule",
-      },
-    ],
-    "Digitalisierung und 3D-Modellierung": [
-      {
-        // PFLICHTFELDER
-        name: "Photogrammetrie",
-        kp: 6,
-        kategorie: "wahlmodule",
-      },
-      {
-        // PFLICHTFELDER
-        name: "Geodätische Messtechnik und Laserscanning",
-        kp: 6,
-        kategorie: "wahlmodule",
-      },
-      {
-        // PFLICHTFELDER
-        name: "Bildverarbeitung",
-        kp: 3,
-        kategorie: "wahlmodule",
-      },
-    ],
-    "GIS und Kartografie": [
-      {
-        // PFLICHTFELDER
-        name: "Kartografie II",
-        kp: 6,
-        kategorie: "wahlmodule",
-      },
-      {
-        // PFLICHTFELDER
-        name: "Geoinformationstechnologien und -analysen",
-        kp: 6,
-        kategorie: "wahlmodule",
-      },
-      {
-        // PFLICHTFELDER
-        name: "Projekt GIS & Kartografie",
-        kp: 3,
-        kategorie: "wahlmodule",
-      },
-    ],
-    "Raum- und Umweltplanung": [
-      {
-        // PFLICHTFELDER
-        name: "Umweltplanung",
-        kp: 3,
-        kategorie: "wahlmodule",
-      },
-      {
-        // PFLICHTFELDER
-        name: "Umweltverträglichkeitsprüfung",
-        kp: 3,
-        kategorie: "wahlmodule",
-      },
-      {
-        // PFLICHTFELDER
-        name: "Integrierte Raumentwicklung in Städten und Quartieren",
-        kp: 6,
-        kategorie: "wahlmodule",
-      },
-      {
-        // PFLICHTFELDER
-        name: "Angewandte Planung zur nachhaltigen Siedlungsentwicklung",
-        kp: 3,
-        kategorie: "wahlmodule",
-      },
-    ],
-    Verkehrssysteme: [
-      {
-        // PFLICHTFELDER
-        name: "Verkehrsplanung",
-        kp: 3,
-        kategorie: "wahlmodule",
-      },
-      {
-        // PFLICHTFELDER
-        name: "Projektübung Verkehr",
-        kp: 6,
-        kategorie: "wahlmodule",
-      },
-      {
-        // PFLICHTFELDER
-        name: "Public transport and railways",
-        kp: 3,
-        kategorie: "wahlmodule",
-      },
-      {
-        // PFLICHTFELDER
-        name: "Road Transport Systems",
-        kp: 3,
-        kategorie: "wahlmodule",
-      },
-    ],
-    Netzinfrastrukturen: [
-      {
-        // PFLICHTFELDER
-        name: "Einführung in elektrische Energiesysteme",
-        kp: 2,
-        kategorie: "wahlmodule",
-      },
-      {
-        // PFLICHTFELDER
-        name: "Siedlungswasserwirtschaft GZ",
-        kp: 6,
-        kategorie: "wahlmodule",
-      },
-      {
-        // PFLICHTFELDER
-        name: "Strasseninfrastruktur",
-        kp: 3,
-        kategorie: "wahlmodule",
-      },
-      {
-        // PFLICHTFELDER
-        name: "Bahninfrastrukturen 1",
-        kp: 2,
-        kategorie: "wahlmodule",
-      },
-      {
-        // PFLICHTFELDER
-        name: "Perspekt. auf Landschaft und urbane Transf. II",
-        kp: 2,
-        kategorie: "wahlmodule",
-      },
-    ],
+window.RIGModuleData = [
+  // Geodäsie und Satellitennavigation
+  {
+    // PFLICHTFELDER
+    name: "Physikalische und kinematische Geodäsie",
+    kp: 6,
+    kategorie: "wahlmodule",
+    bereich: "Geodäsie und Satellitennavigation",
   },
-};
+  {
+    // PFLICHTFELDER
+    name: "Globale Satellitennavigationssysteme",
+    kp: 3,
+    kategorie: "wahlmodule",
+    bereich: "Geodäsie und Satellitennavigation",
+  },
+  {
+    // PFLICHTFELDER
+    name: "Geodätische Datenanalyse",
+    kp: 3,
+    kategorie: "wahlmodule",
+    bereich: "Geodäsie und Satellitennavigation",
+  },
+  {
+    // PFLICHTFELDER
+    name: "Navigation",
+    kp: 3,
+    kategorie: "wahlmodule",
+    bereich: "Geodäsie und Satellitennavigation",
+  },
+  // Digitalisierung und 3D-Modellierung
+  {
+    // PFLICHTFELDER
+    name: "Photogrammetrie",
+    kp: 6,
+    kategorie: "wahlmodule",
+    bereich: "Digitalisierung und 3D-Modellierung",
+  },
+  {
+    // PFLICHTFELDER
+    name: "Geodätische Messtechnik und Laserscanning",
+    kp: 6,
+    kategorie: "wahlmodule",
+    bereich: "Digitalisierung und 3D-Modellierung",
+  },
+  {
+    // PFLICHTFELDER
+    name: "Bildverarbeitung",
+    kp: 3,
+    kategorie: "wahlmodule",
+    bereich: "Digitalisierung und 3D-Modellierung",
+  },
+  // GIS und Kartografie
+  {
+    // PFLICHTFELDER
+    name: "Kartografie II",
+    kp: 6,
+    kategorie: "wahlmodule",
+    bereich: "GIS und Kartografie",
+  },
+  {
+    // PFLICHTFELDER
+    name: "Geoinformationstechnologien und -analysen",
+    kp: 6,
+    kategorie: "wahlmodule",
+    bereich: "GIS und Kartografie",
+  },
+  {
+    // PFLICHTFELDER
+    name: "Projekt GIS & Kartografie",
+    kp: 3,
+    kategorie: "wahlmodule",
+    bereich: "GIS und Kartografie",
+  },
+  // Raum- und Umweltplanung
+  {
+    // PFLICHTFELDER
+    name: "Umweltplanung",
+    kp: 3,
+    kategorie: "wahlmodule",
+    bereich: "Raum- und Umweltplanung",
+  },
+  {
+    // PFLICHTFELDER
+    name: "Umweltverträglichkeitsprüfung",
+    kp: 3,
+    kategorie: "wahlmodule",
+    bereich: "Raum- und Umweltplanung",
+  },
+  {
+    // PFLICHTFELDER
+    name: "Integrierte Raumentwicklung in Städten und Quartieren",
+    kp: 6,
+    kategorie: "wahlmodule",
+    bereich: "Raum- und Umweltplanung",
+  },
+  {
+    // PFLICHTFELDER
+    name: "Angewandte Planung zur nachhaltigen Siedlungsentwicklung",
+    kp: 3,
+    kategorie: "wahlmodule",
+    bereich: "Raum- und Umweltplanung",
+  },
+  // Verkehrssysteme
+  {
+    // PFLICHTFELDER
+    name: "Verkehrsplanung",
+    kp: 3,
+    kategorie: "wahlmodule",
+    bereich: "Verkehrssysteme",
+  },
+  {
+    // PFLICHTFELDER
+    name: "Projektübung Verkehr",
+    kp: 6,
+    kategorie: "wahlmodule",
+    bereich: "Verkehrssysteme",
+  },
+  {
+    // PFLICHTFELDER
+    name: "Public transport and railways",
+    kp: 3,
+    kategorie: "wahlmodule",
+    bereich: "Verkehrssysteme",
+  },
+  {
+    // PFLICHTFELDER
+    name: "Road Transport Systems",
+    kp: 3,
+    kategorie: "wahlmodule",
+    bereich: "Verkehrssysteme",
+  },
+  // Netzinfrastrukturen
+  {
+    // PFLICHTFELDER
+    name: "Einführung in elektrische Energiesysteme",
+    kp: 2,
+    kategorie: "wahlmodule",
+    bereich: "Netzinfrastrukturen",
+  },
+  {
+    // PFLICHTFELDER
+    name: "Siedlungswasserwirtschaft GZ",
+    kp: 6,
+    kategorie: "wahlmodule",
+    bereich: "Netzinfrastrukturen",
+  },
+  {
+    // PFLICHTFELDER
+    name: "Strasseninfrastruktur",
+    kp: 3,
+    kategorie: "wahlmodule",
+    bereich: "Netzinfrastrukturen",
+  },
+  {
+    // PFLICHTFELDER
+    name: "Bahninfrastrukturen 1",
+    kp: 2,
+    kategorie: "wahlmodule",
+    bereich: "Netzinfrastrukturen",
+  },
+  {
+    // PFLICHTFELDER
+    name: "Perspekt. auf Landschaft und urbane Transf. II",
+    kp: 2,
+    kategorie: "wahlmodule",
+    bereich: "Netzinfrastrukturen",
+  },
+];
