1df4f35cebf56310947ca1c6ba8b8cea676a52e9
Kristian Lubina <kristian.lubina@outlook.com>
2025-11-13 21:07:43 +0100

    Move specialisation modules to separate data file

---
+++ study-visualization/program-specific/major-minor/uzh-polisci/data/specialisationmodule-data.js
@@ -0,0 +1,101 @@
+/* ==== POLITIKWISSENSCHAFT SPECIALISATION MODULE DATA ==== */
+// Specialisation modules extracted from Wahlmodule für Politikwissenschaft (HS 2025 + FS 2024)
+
+window.SpecialisationModuleData = [
+  // hs2025 Specialisation modules
+  {
+    name: "Specialisation: Agenda Setting and the Media...",
+    ects: 6,
+    standardcategory: "Wahlmodule",
+    semester: 1,
+  },
+  {
+    name: "Specialisation: International Economic Institutions",
+    ects: 6,
+    standardcategory: "Wahlmodule",
+    semester: 1,
+  },
+  {
+    name: "Specialisation: Parliaments and Democracy",
+    ects: 6,
+    standardcategory: "Wahlmodule",
+    semester: 1,
+  },
+  {
+    name: "Specialisation: Political Mobilization and Violence",
+    ects: 6,
+    standardcategory: "Wahlmodule",
+    semester: 1,
+  },
+  {
+    name: "Specialisation: Political Participation",
+    ects: 6,
+    standardcategory: "Wahlmodule",
+    semester: 1,
+  },
+  {
+    name: "Specialisation: Reforming Representative Democracy",
+    ects: 6,
+    standardcategory: "Wahlmodule",
+    semester: 1,
+  },
+  {
+    name: "Spezialisierung: Debatten, Diskurse, Dialoge...",
+    ects: 6,
+    standardcategory: "Wahlmodule",
+    semester: 1,
+  },
+  {
+    name: "Spezialisierung: Direkte Demokratie Schweiz im 21. Jahrhundert",
+    ects: 6,
+    standardcategory: "Wahlmodule",
+    semester: 1,
+  },
+  {
+    name: "Spezialisierung: Frieden und Konflikt in den Internationalen Beziehungen",
+    ects: 6,
+    standardcategory: "Wahlmodule",
+    semester: 1,
+  },
+  {
+    name: "Spezialisierung: Governance und Demokratie",
+    ects: 6,
+    standardcategory: "Wahlmodule",
+    semester: 1,
+  },
+  {
+    name: "Spezialisierung: Macht Politik einen Unterschied?...",
+    ects: 6,
+    standardcategory: "Wahlmodule",
+    semester: 1,
+  },
+  {
+    name: "Spezialisierung: Staat und Gewalt. Problem und Lösungsansätze",
+    ects: 6,
+    standardcategory: "Wahlmodule",
+    semester: 1,
+  },
+  {
+    name: "Spezialisierung: Technologischer Wandel, soziale Transformation und politisches Verhalten",
+    ects: 6,
+    standardcategory: "Wahlmodule",
+    semester: 1,
+  },
+
+  // fs2024 Specialisation modules
+  {
+    name: "Specialisation: Elections, Party Competition and Representation in Europe",
+    ects: 6,
+    standardcategory: "Wahlmodule",
+    semester: 2,
+  },
+  {
+    name: "Spezialisierung: Citizens democratic commitment in times of crisis",
+    ects: 6,
+    standardcategory: "Wahlmodule",
+    semester: 2,
+  },
+];
+
+// Export für wahlmodule.js - der Code erkennt nur spezifische Variablennamen
+window.WahlmoduleData = window.SpecialisationModuleData;
