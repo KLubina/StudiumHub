530967eb6e33604674a74897e66e6d2a058d3796
Kristian Lubina <kristian.lubina@outlook.com>
2025-11-13 21:02:21 +0100

    Add seminar data for UZH political science program

---
+++ study-visualization/program-specific/major-minor/uzh-polisci/data/seminar-data.js
@@ -0,0 +1,74 @@
+/* ==== POLITIKWISSENSCHAFT SEMINAR DATA ==== */
+// Seminare (i. d. R. 6 ECTS)
+// Hinweis: Export alias für Wahlmodule-System, damit als auswählbare Liste genutzt werden kann.
+
+window.PolisciSeminarData = [
+  {
+    name: "Aktuelle Themen der Politikwissenschaft",
+    ects: 6,
+    standardcategory: "Vertiefungsmodule",
+  },
+  {
+    name: "Entwicklung und Nachhaltigkeit",
+    ects: 6,
+    standardcategory: "Vertiefungsmodule",
+  },
+  {
+    name: "Europäische Politik",
+    ects: 6,
+    standardcategory: "Vertiefungsmodule",
+  },
+  {
+    name: "Governance",
+    ects: 6,
+    standardcategory: "Vertiefungsmodule",
+  },
+  {
+    name: "Sicherheit und Konflikt",
+    ects: 6,
+    standardcategory: "Vertiefungsmodule",
+  },
+  {
+    name: "Vergleichende und Internationale Politische Ökonomie",
+    ects: 6,
+    standardcategory: "Vertiefungsmodule",
+  },
+  {
+    name: "Demokratieforschung",
+    ects: 6,
+    standardcategory: "Vertiefungsmodule",
+  },
+  {
+    name: "Internationale Politik",
+    ects: 6,
+    standardcategory: "Vertiefungsmodule",
+  },
+  {
+    name: "Politikprozesse und Public Policy",
+    ects: 6,
+    standardcategory: "Vertiefungsmodule",
+  },
+  {
+    name: "Politisches Verhalten",
+    ects: 6,
+    standardcategory: "Vertiefungsmodule",
+  },
+  {
+    name: "Public Choice",
+    ects: 6,
+    standardcategory: "Vertiefungsmodule",
+  },
+  {
+    name: "Schweizer Politik",
+    ects: 6,
+    standardcategory: "Vertiefungsmodule",
+  },
+  {
+    name: "Vergleichende Politik",
+    ects: 6,
+    standardcategory: "Vertiefungsmodule",
+  },
+];
+
+// Export für wahlmodule.js - der Code erkennt nur spezifische Variablennamen
+window.WahlmoduleData = window.PolisciSeminarData;
