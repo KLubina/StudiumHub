6966038ab005df0d705cfb127fa87398b139a985
Kristian Lubina <kristian.lubina@outlook.com>
2025-11-13 21:22:53 +0100

    Remove prefixes from specialisation module names

--- study-visualization/program-specific/major-minor/uzh-polisci/data/specialisationmodule-data.js
+++ study-visualization/program-specific/major-minor/uzh-polisci/data/specialisationmodule-data.js
@@ -4,79 +4,79 @@
 window.SpecialisationModuleData = [
   // hs2025 Specialisation modules
   {
-    name: "Specialisation: Agenda Setting and the Media...",
+    name: "Agenda Setting and the Media...",
     ects: 6,
     standardcategory: "Wahlmodule",
     semester: 1,
   },
   {
-    name: "Specialisation: International Economic Institutions",
+    name: "International Economic Institutions",
     ects: 6,
     standardcategory: "Wahlmodule",
     semester: 1,
   },
   {
-    name: "Specialisation: Parliaments and Democracy",
+    name: "Parliaments and Democracy",
     ects: 6,
     standardcategory: "Wahlmodule",
     semester: 1,
   },
   {
-    name: "Specialisation: Political Mobilization and Violence",
+    name: "Political Mobilization and Violence",
     ects: 6,
     standardcategory: "Wahlmodule",
     semester: 1,
   },
   {
-    name: "Specialisation: Political Participation",
+    name: "Political Participation",
     ects: 6,
     standardcategory: "Wahlmodule",
     semester: 1,
   },
   {
-    name: "Specialisation: Reforming Representative Democracy",
+    name: "Reforming Representative Democracy",
     ects: 6,
     standardcategory: "Wahlmodule",
     semester: 1,
   },
   {
-    name: "Spezialisierung: Debatten, Diskurse, Dialoge...",
+    name: "Debatten, Diskurse, Dialoge...",
     ects: 6,
     standardcategory: "Wahlmodule",
     semester: 1,
   },
   {
-    name: "Spezialisierung: Direkte Demokratie Schweiz im 21. Jahrhundert",
+    name: "Direkte Demokratie Schweiz im 21. Jahrhundert",
     ects: 6,
     standardcategory: "Wahlmodule",
     semester: 1,
   },
   {
-    name: "Spezialisierung: Frieden und Konflikt in den Internationalen Beziehungen",
+    name: "Frieden und Konflikt in den Internationalen Beziehungen",
     ects: 6,
     standardcategory: "Wahlmodule",
     semester: 1,
   },
   {
-    name: "Spezialisierung: Governance und Demokratie",
+    name: "Governance und Demokratie",
     ects: 6,
     standardcategory: "Wahlmodule",
     semester: 1,
   },
   {
-    name: "Spezialisierung: Macht Politik einen Unterschied?...",
+    name: "Macht Politik einen Unterschied?...",
     ects: 6,
     standardcategory: "Wahlmodule",
     semester: 1,
   },
   {
-    name: "Spezialisierung: Staat und Gewalt. Problem und Lösungsansätze",
+    name: "Staat und Gewalt. Problem und Lösungsansätze",
     ects: 6,
     standardcategory: "Wahlmodule",
     semester: 1,
   },
   {
-    name: "Spezialisierung: Technologischer Wandel, soziale Transformation und politisches Verhalten",
+    name: "Technologischer Wandel, soziale Transformation und politisches Verhalten",
     ects: 6,
     standardcategory: "Wahlmodule",
     semester: 1,
@@ -84,13 +84,13 @@ window.SpecialisationModuleData = [
 
   // fs2024 Specialisation modules
   {
-    name: "Specialisation: Elections, Party Competition and Representation in Europe",
+    name: "Elections, Party Competition and Representation in Europe",
     ects: 6,
     standardcategory: "Wahlmodule",
     semester: 2,
   },
   {
-    name: "Spezialisierung: Citizens democratic commitment in times of crisis",
+    name: "Citizens democratic commitment in times of crisis",
     ects: 6,
     standardcategory: "Wahlmodule",
     semester: 2,
