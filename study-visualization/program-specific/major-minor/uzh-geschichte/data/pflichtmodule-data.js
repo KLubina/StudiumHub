69fde62e15390cd76cea10a04aac43bbade14c4e
Kristian Lubina <kristian.lubina@outlook.com>
2025-11-14 14:29:09 +0100

    Update Geschichte Pflichtmodule and add Polisci details

--- study-visualization/program-specific/major-minor/uzh-geschichte/data/pflichtmodule-data.js
+++ study-visualization/program-specific/major-minor/uzh-geschichte/data/pflichtmodule-data.js
@@ -1,93 +1,75 @@
-/* ==== GESCHICHTE PFLICHTMODULE DATA ==== */
-// Pflichtmodule für Geschichte (HS 2025 + FS 2026)
-
 window.GeschichtePflichtmoduleData = [
-  // hs2025 Pflichtmodule
+  // Jahr 1
   {
-    name: "Bachelorarbeit",
-    ects: 15,
+    name: "Einführungsvorlesung Herbst",
+    ects: 3,
     standardcategory: "Pflichtmodule",
     semester: 1,
-    year: 3
+    year: 1
   },
   {
     name: "Basismodul I",
     ects: 9,
     standardcategory: "Pflichtmodule",
-    semester: 1
-  },
-  {
-    name: "Basismodul II",
-    ects: 9,
-    standardcategory: "Pflichtmodule",
-    semester: 1
-  },
-  {
-    name: "Einführungsvorlesung Herbst",
-    ects: 3,
-    standardcategory: "Pflichtmodule",
-    semester: 1
+    semester: 1,
+    year: 1
   },
   {
     name: "Grundlagen Latein",
     ects: 6,
     standardcategory: "Pflichtmodule",
-    semester: 1
-  },
-  {
-    name: "Methoden und Theorie",
-    ects: 9,
-    standardcategory: "Pflichtmodule",
-    semester: 1
-  },
-  {
-    name: "Mündliche Prüfung",
-    ects: 9,
-    standardcategory: "Pflichtmodule",
     semester: 1,
-    year: 3
-  },
-  // fs2026 Pflichtmodule
-  {
-    name: "Bachelorarbeit",
-    ects: 15,
-    standardcategory: "Pflichtmodule",
-    semester: 2,
-    year: 3
-  },
-  {
-    name: "Basismodul I",
-    ects: 9,
-    standardcategory: "Pflichtmodule",
-    semester: 2
+    year: 1
   },
   {
     name: "Basismodul II",
     ects: 9,
     standardcategory: "Pflichtmodule",
-    semester: 2
+    semester: 2,
+    year: 1
+  },
+  {
+    name: "Grundlagen Latein",
+    ects: 6,
+    standardcategory: "Pflichtmodule",
+    semester: 2,
+    year: 1
   },
   {
     name: "Einführungsvorlesung Frühling",
     ects: 3,
     standardcategory: "Pflichtmodule",
-    semester: 2
+    semester: 2,
+    year: 1
   },
-  {
-    name: "Grundlagen Latein",
-    ects: 6,
-    standardcategory: "Pflichtmodule",
-    semester: 2
-  },
+
+  // Jahr 2
   {
     name: "Methoden und Theorie",
     ects: 9,
     standardcategory: "Pflichtmodule",
-    semester: 2
+    semester: 2,
+    year: 2
   },
   {
     name: "Mündliche Prüfung",
     ects: 9,
+    standardcategory: "Pflichtmodule",
+    semester: 2,
+    year: 2
+  },
+
+  // Jahr 3
+  {
+    name: "Methoden und Theorie",
+    ects: 9,
+    standardcategory: "Pflichtmodule",
+    semester: 1,
+    year: 3
+  },
+  {
+    name: "Bachelorarbeit",
+    ects: 15,
     standardcategory: "Pflichtmodule",
     semester: 2,
     year: 3
---
+++ study-visualization/program-specific/major-minor/uzh-polisci/data/pflichtmodule-details.js
@@ -0,0 +1,25 @@
+Politische Systeme und Theorien I
+https://studentservices.uzh.ch/uzh/anonym/vvz/index.html?sap-language=DE&sap-ui-language=DE#/details/2025/003/E/51284114/50000007/Philosophische%2520Fakult%25C3%25A4t/50890576/Bachelor%2520of%2520Arts%2520in%2520Sozialwissenschaften%2520(RVO19)/50893129/Politikwissenschaft///50942114/Politische%2520Systeme%2520und%2520Theorien%2520I
+Die Vorlesung "Politische Systeme und Theorien" führt die Studierenden in die Politikwissenschaft ein und gibt einen Überblick über politische Institutionen, politische Akteure und ihr Zusammenspiel.
+Die Vorlesung beginnt mit einer Einführung in die Politikwissenschaft und führt dabei verschiedene theoretische Ansätze an. 
+Im zweiten Teil geht es um grundlegende Begriffe wie die Autorität, Legitimität, Macht und Souveränität des Staates. 
+In einem dritten Teil werden Herrschaftsformen und politische Systeme wie Demokratie und Autokratie behandelt. 
+Daran schliesst ein vierter Teil über demokratische Institutionen an. 
+Der fünfte Teil behandelt strategische Interaktionen zwischen politischen Akteuren und stellt dabei auch verschiedene Analyse-Tools vor.
+
+Einführung in die Schweizer Politik
+https://studentservices.uzh.ch/uzh/anonym/vvz/index.html?sap-language=DE&sap-ui-language=DE#/details/2025/003/SM/50942116/50000007/Philosophische%2520Fakult%25C3%25A4t/50890576/Bachelor%2520of%2520Arts%2520in%2520Sozialwissenschaften%2520(RVO19)/50893129/Politikwissenschaft
+Diese Veranstaltung bietet eine Einführung in die Institutionen und die Funktionsweise des schweizerischen politischen Systems und dessen Eigenheiten im internationalen Vergleich. Dazu gehören der stark ausgeprägte Föderalismus, die direkte Demokratie, die Konkordanz und das weltweit einzigartige, siebenköpfige Regierungskollegium. Die Vorlesung geht auf diese Strukturmerkmale des politischen Systems ein und zeigt auf, wie sie miteinander zusammenhängen und sich teilweise gegenseitig bedingen. Zudem werden theoretische Interpretationen vorgestellt und diskutiert, mit denen es möglich ist, die Eigenheiten des politischen Systems der Schweiz in grösseren politikwissenschaftlichen Zusammenhängen zu verstehen.
+
+Einführung Methoden und Statistik
+https://studentservices.uzh.ch/uzh/anonym/vvz/index.html?sap-language=DE&sap-ui-language=DE#/details/2025/003/SM/50942117/50000007/Philosophische%2520Fakult%25C3%25A4t/50890576/Bachelor%2520of%2520Arts%2520in%2520Sozialwissenschaften%2520(RVO19)/50893129/Politikwissenschaft
+Was ist (politische) Wissenschaft? Was unterscheidet wissenschaftliche Erkenntnisse von der Laienbeobachtung? Wie kommen Politikwissenschaftler zu den Erkenntnissen, die man in Lehrbüchern findet. Themen sind: Wissenschaftsphilosophie, Messung, Forschungsdesigns zur Beschreibung, Hypothesentest/Generierung und kausale Inferenz sowie Methoden der Datenerhebung. 
+
+Aufbaukurs Methoden und Statistik
+https://studentservices.uzh.ch/uzh/anonym/vvz/index.html?sap-language=DE&sap-ui-language=DE#/details/2024/004/SM/50942118/50000007/Philosophische%2520Fakult%25C3%25A4t/50890576/Bachelor%2520of%2520Arts%2520in%2520Sozialwissenschaften%2520(RVO19)/50893129/Politikwissenschaft
+Aufbauend auf dem Einführungskurs werden in diesem Kurs fortgeschrittene Methoden und Modelle behandelt, die es den Studierenden ermöglichen, kausale Inferenzen zu generieren. Der Schwerpunkt liegt insbesondere auf dem linearen Regressionsmodell. Die Studierenden erlernen die Annahmen, unter denen die Ergebnisse eines Regressionsmodells für spezifische Zwecke nützlich sind, sowiedie Fähigkeit, kausale Schlussfolgerungen aus Beobachtungsdaten zu ziehen.
+
+Politische Systeme und Theorien II
+https://studentservices.uzh.ch/uzh/anonym/vvz/index.html?sap-language=DE&sap-ui-language=DE#/details/2024/004/SM/50942115/50000007/Philosophische%2520Fakult%25C3%25A4t/50890576/Bachelor%2520of%2520Arts%2520in%2520Sozialwissenschaften%2520(RVO19)/50893129/Politikwissenschaft
+Das Modul gibt eine vertiefte Einführung in Theorien, Systeme und Prozesse der Politikwissenschaft, sowie in ihre Teildisziplinen. Behandelt werden u.a.
+folgende Themen: Politisches Handeln, Werte, Interessen, politische Partizipation, Parteien und Parteiensysteme, soziale Bewegungen, Interessengruppen und Medien. Dabei werden auch verschiedene Politikfelder behandelt: Gesellschaftspolitik, Migration, Umwelt, Entwicklung, Gender, Wirtschaftspolitik, Wohlfahrtstaat, Krieg, Frieden, internationale Regime und Organisationen, Globalisierung.
