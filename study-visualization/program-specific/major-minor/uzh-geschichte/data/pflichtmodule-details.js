9e7e62c57764625124d3a3302d9a2979a760076e
Kristian Lubina <kristian.lubina@outlook.com>
2025-11-15 19:49:14 +0100

    Add Pflichtmodule details for UZH Geschichte

---
+++ study-visualization/program-specific/major-minor/uzh-geschichte/data/pflichtmodule-details.js
@@ -0,0 +1,32 @@
+window.StudiengangModuleDetails = {
+  "Basismodul I": {
+    kurzbeschreibung: "Einführung in Recherchetechniken und wissenschaftliches Arbeiten",
+    inhalt: "Das Basismodul I besteht aus Proseminar 1 und Proseminar 2, die ein übergreifendes Thema behandeln. Im Proseminar 1 werden Recherchetechniken, der Umgang mit wissenschaftlicher Literatur sowie wissenschaftliches Schreiben und Vortragen vermittelt. Im Proseminar 2 steht die wissenschaftliche Beschäftigung mit Quellen sowie das Verfassen einer Proseminararbeit im Zentrum.",
+    link: "https://studentservices.uzh.ch/uzh/anonym/vvz/index.html?sap-language=DE&sap-ui-language=DE#/details/2025/003/SM/50942000/50000007/Philosophische%2520Fakult%25C3%25A4t/50890575/Bachelor%2520of%2520Arts%2520(RVO19)/50891442/Geschichte",
+  },
+  "Basismodul II": {
+    kurzbeschreibung: "Grundlagen der Geschichtswissenschaft und Historiographie",
+    inhalt: "Die Studierenden kennen die wichtigsten Methoden und Theorien der Geschichtswissenschaft und erwerben grundlegende Kenntnisse der Historiographiegeschichte. Zudem kennen sie gängige Textsorten der Geschichtswissenschaft und können Standards und Techniken des wissenschaftlichen Schreibens anwenden.",
+    link: "https://studentservices.uzh.ch/uzh/anonym/vvz/index.html?sap-language=DE&sap-ui-language=DE#/details/2025/003/SM/50942002/50000007/Philosophische%2520Fakult%25C3%25A4t/50890575/Bachelor%2520of%2520Arts%2520(RVO19)/50891442/Geschichte",
+  },
+  "Grundlagen Latein": {
+    kurzbeschreibung: "Einführung in die lateinische Sprache und römische Kultur",
+    inhalt: "In diesem Kurs werden anhand eines Lehrmittels die Grundlagen der lateinischen Sprache in Formenlehre, Syntax und Vokabular vermittelt. Im Laufe des Kurses kommt vermehrt die Lektüre einfacher Originaltexte hinzu. Parallel zum Sprach- und Lektüreunterricht werden wichtige Aspekte der römischen und griechischen Kultur behandelt. Die sprachlichen Kenntnisse entsprechen (in einer für die Alten Sprachen modifizierten Skala) A2/B1 (gesicherte Lateinkenntnisse). Das Modul \"Grundlagen Latein\" kann in zwei Varianten absolviert werden: als 1-semestriger Kurs (fast track), der jedes Herbstsemester angeboten wird, und als 2-semestriger Kurs, mit dem in jedem Semester (HS und FS) begonnen werden kann.",
+    link: "https://studentservices.uzh.ch/uzh/anonym/vvz/index.html?sap-language=DE&sap-ui-language=DE#/details/2025/003/SM/50945514/50000007/Philosophische%2520Fakult%25C3%25A4t/50890575/Bachelor%2520of%2520Arts%2520(RVO19)/50891442/Geschichte",
+  },
+  "Einführungsvorlesung Herbst": {
+    kurzbeschreibung: "Einführung in grundlegende Themen der Geschichtswissenschaft",
+    inhalt: "Das Modul bietet eine Einführung in grundlegende Themen der Geschichtswissenschaft. Die Studierenden können das in der Vorlesung vermittelte Fachwissen zu grundlegenden Themen der Geschichtswissenschaft reflektiert wiedergeben.",
+    link: "https://studentservices.uzh.ch/uzh/anonym/vvz/index.html?sap-language=DE&sap-ui-language=DE#/details/2025/003/SM/50942001/50000007/Philosophische%2520Fakult%25C3%25A4t/50890575/Bachelor%2520of%2520Arts%2520(RVO19)/50891442/Geschichte",
+  },
+  "Methoden und Theorie": {
+    kurzbeschreibung: "Intensive Auseinandersetzung mit Methoden und Theorien der Geschichtswissenschaft",
+    inhalt: "Das Seminar \"Methoden und Theorie\" ermöglicht eine intensive Auseinandersetzung mit Methoden und Theorien der Geschichtswissenschaft. Die Studierenden können die im Seminar behandelten methodisch-theoretischen Konzepte der Geschichtswissenschaft in der eigenen Arbeit anwenden.",
+    link: "https://studentservices.uzh.ch/uzh/anonym/vvz/index.html?sap-language=DE&sap-ui-language=DE#/details/2025/003/SM/50941993/50000007/Philosophische%2520Fakult%25C3%25A4t/50890575/Bachelor%2520of%2520Arts%2520(RVO19)/50891442/Geschichte",
+  },
+  "Mündliche Prüfung": {
+    kurzbeschreibung: "Prüfung wichtiger geschichtswissenschaftlicher Werke",
+    inhalt: "Die Studierenden machen sich mit wichtigen geschichtswissenschaftlichen Werken vertraut. Sie sind in der Lage, die Inhalte der gelesenen Titel strukturiert darzustellen, in den historiographischen Kontext einzuordnen und mit dem Prüfenden bzw. der Prüfenden reflektiert über die Inhalte zu diskutieren.",
+    link: "https://studentservices.uzh.ch/uzh/anonym/vvz/index.html?sap-language=DE&sap-ui-language=DE#/details/2025/003/SM/50941994/50000007/Philosophische%2520Fakult%25C3%25A4t/50890575/Bachelor%2520of%2520Arts%2520(RVO19)/50891442/Geschichte",
+  },
+};
