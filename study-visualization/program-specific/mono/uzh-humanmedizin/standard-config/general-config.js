ff4722731bf93daa55aec01c8f51d4ed3e5a5ee2
Kristian Lubina <kristian.lubina@outlook.com>
2025-11-15 23:45:51 +0100

    Add subtitleHtml with curriculum link to config

--- study-visualization/program-specific/mono/uzh-humanmedizin/standard-config/general-config.js
+++ study-visualization/program-specific/mono/uzh-humanmedizin/standard-config/general-config.js
@@ -4,5 +4,6 @@
 window.StudiengangGeneralConfig = {
     // Program identification
     title: "Bachelor Humanmedizin",
-    curriculum: "https://www.med.uzh.ch/dam/jcr:ffffffff-c4b3-d46e-0000-000016d79dfa/curriculum_med_2012.pdf"
+    // Optional HTML subtitle (renders clickable links when provided)
+    subtitleHtml: `<a href="https://www.med.uzh.ch/dam/jcr:ffffffff-c4b3-d46e-0000-000016d79dfa/curriculum_med_2012.pdf" target="_blank" rel="noopener">Curriculum</a>`
 };
