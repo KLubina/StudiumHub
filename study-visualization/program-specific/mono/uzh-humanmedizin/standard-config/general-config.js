1344642a9fac8e1bf5edb47a2314b5d8d586f7b4
Kristian Lubina <kristian.lubina@outlook.com>
2025-11-16 20:50:16 +0100

    Add NC Beispielaufgaben 2025 link to subtitleHtml

--- study-visualization/program-specific/mono/uzh-humanmedizin/standard-config/general-config.js
+++ study-visualization/program-specific/mono/uzh-humanmedizin/standard-config/general-config.js
@@ -5,5 +5,5 @@ window.StudiengangGeneralConfig = {
     // Program identification
     title: "Bachelor Humanmedizin",
     // Optional HTML subtitle (renders clickable links when provided)
-    subtitleHtml: `<a href="https://www.med.uzh.ch/dam/jcr:ffffffff-c4b3-d46e-0000-000016d79dfa/curriculum_med_2012.pdf" target="_blank" rel="noopener">Curriculum</a>`
+    subtitleHtml: `<a href="https://www.med.uzh.ch/dam/jcr:ffffffff-c4b3-d46e-0000-000016d79dfa/curriculum_med_2012.pdf" target="_blank" rel="noopener">Curriculum</a> | <a href="https://www.swissuniversities.ch/fileadmin/swissuniversities/Dokumente/Lehre/Medizin/Beispielaufgaben2025.pdf" target="_blank" rel="noopener">NC Beispielaufgaben 2025</a>`
 };
