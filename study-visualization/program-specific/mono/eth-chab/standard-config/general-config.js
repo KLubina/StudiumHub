63235b3a2477a798f6dbd8081b4804131de25f91
Kristian Lubina <kristian.lubina@outlook.com>
2025-11-22 23:58:34 +0100

    Replace curriculumLink with subtitleHtml in config

--- study-visualization/program-specific/mono/eth-chab/standard-config/general-config.js
+++ study-visualization/program-specific/mono/eth-chab/standard-config/general-config.js
@@ -5,6 +5,6 @@ window.StudiengangGeneralConfig = {
     // Program identification
     title: "BSc Chemieingenieurwissenschaften",
 
-    // Link to official curriculum
-    curriculumLink: "https://ethz.ch/content/dam/ethz/special-interest/chab/chab-dept/studies/documents/C_CI/HS25_BSc_CI.pdf"
+    // Optional HTML subtitle (renders clickable links when provided)
+    subtitleHtml: `<a href="https://ethz.ch/content/dam/ethz/special-interest/chab/chab-dept/studies/documents/C_CI/HS25_BSc_CI.pdf" target="_blank" rel="noopener">Curriculum PDF</a>`
 };
