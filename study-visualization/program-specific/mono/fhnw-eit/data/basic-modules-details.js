7b99eaf7943eff2cb7028ac97ee3e7ae62fc896b
Kristian Lubina <kristian.lubina@outlook.com>
2025-11-23 15:47:41 +0100

    Add FHNW EIT basic modules details data file
    Introduces a new JavaScript file containing module names and their corresponding FHNW website links for the Embedded Systems Design profile. This data will be used for displaying detailed module information in the study visualization.

---
+++ study-visualization/program-specific/mono/fhnw-eit/data/basic-modules-details.js
@@ -0,0 +1,68 @@
+/* ==== FHNW EIT MODULE DETAILS - LINK INFORMATIONEN ==== */
+/* Links zu den Modulbeschreibungen auf der FHNW Website */
+/* Vertiefungsprofil: Embedded Systems Design */
+
+window.ModuleDetails = {
+  // #region 1. Semester
+  "Projekt 1": "https://www.fhnw.ch/de/studium/module/9169675",
+  "Objektorientierte Programmierung 1": "https://www.fhnw.ch/de/studium/module/6007993",
+  "Wärme und Strahlung": "https://www.fhnw.ch/de/studium/module/9063957",
+  "Allgemeine Elektrotechnik 1": "https://www.fhnw.ch/de/studium/module/6007771",
+  "Konvergenz Elektrotechnik": "https://www.fhnw.ch/de/studium/module/9041293",
+  "Analysis 1": "https://www.fhnw.ch/de/studium/module/6008327",
+  "Lineare Algebra 1": "https://www.fhnw.ch/de/studium/module/6008325",
+  "Grundlagenlabor 1": "https://www.fhnw.ch/de/studium/module/6010242",
+  // #endregion
+
+  // #region 2. Semester
+  "Algebra": "https://www.fhnw.ch/de/studium/module/6008331",
+  "Analysis 2": "https://www.fhnw.ch/de/studium/module/6008332",
+  "Mechanik": "https://www.fhnw.ch/de/studium/module/9090504",
+  "Projekt 2": "https://www.fhnw.ch/de/studium/module/9155733",
+  "Grundlagenlabor 2": "https://www.fhnw.ch/de/studium/module/6010244",
+  "Allgemeine Elektrotechnik 2": "https://www.fhnw.ch/de/studium/module/6007772",
+  "Digitaltechnik 1": "https://www.fhnw.ch/de/studium/module/6007847",
+  "Objektorientierte Programmierung 2": "https://www.fhnw.ch/de/studium/module/6007994",
+  // #endregion
+
+  // #region 3. Semester
+  "Grundlagenlabor 3": "https://www.fhnw.ch/de/studium/module/6010245",
+  "Modellieren dynamischer Systeme": "https://www.fhnw.ch/de/studium/module/9067170",
+  "Projekt 3": "https://www.fhnw.ch/de/studium/module/9155749",
+  "Analysis 3": "https://www.fhnw.ch/de/studium/module/6008406",
+  "Elektromagnetismus": "https://www.fhnw.ch/de/studium/module/6013946",
+  "Analogtechnik": "https://www.fhnw.ch/de/studium/module/9102228",
+  "Signalverarbeitung": "https://www.fhnw.ch/de/studium/module/9030028",
+  "Kommunikationstechnik Grundlagen": "https://www.fhnw.ch/de/studium/module/9029168",
+  "Mikrocontroller - Grundlagen": "https://www.fhnw.ch/de/studium/module/9436642",
+  // #endregion
+
+  // #region 4. Semester
+  "Grundlagenlabor 4": "https://www.fhnw.ch/de/studium/module/6010246",
+  "Digitale Signalverarbeitung - Grundlagen": "https://www.fhnw.ch/de/studium/module/9029185",
+  "Digitaltechnik 2": "https://www.fhnw.ch/de/studium/module/6007850",
+  "Integraltransformationen": "https://www.fhnw.ch/de/studium/module/6008408",
+  "Schwingungen und Wellen": "https://www.fhnw.ch/de/studium/module/9093422",
+  "Analoge Schaltungstechnik - Grundlagen": "https://www.fhnw.ch/de/studium/module/9390399",
+  "Projekt 4": "https://www.fhnw.ch/de/studium/module/9155761",
+  // #endregion
+
+  // #region 5. Semester
+  "Projekt 5": "https://www.fhnw.ch/de/studium/module/6018820",
+  "Labor Embedded Systems Design 1": "https://www.fhnw.ch/de/studium/module/9436437",
+  "Kommunikationstechnik": "https://www.fhnw.ch/de/studium/module/9108204",
+  "Digitale Schaltungstechnik": "https://www.fhnw.ch/de/studium/module/9029170",
+  "Digitale Signalverarbeitung": "https://www.fhnw.ch/de/studium/module/9108200",
+  "Microcontroller Systems Programming": "https://www.fhnw.ch/de/studium/module/9416781",
+  "Datennetze": "https://www.fhnw.ch/de/studium/module/9288353",
+  "Workshop Linux und Webtechnologien": "https://www.fhnw.ch/de/studium/module/9434920",
+  // #endregion
+
+  // #region 6. Semester
+  "Projekt 6": "https://www.fhnw.ch/de/studium/module/9004584",
+  "Labor Embedded Systems Design 2": "https://www.fhnw.ch/de/studium/module/9436624",
+  "Digital Communication": "https://www.fhnw.ch/de/studium/module/9107973",
+  "Embedded Systems Software Design": "https://www.fhnw.ch/de/studium/module/6007864",
+  "Analog Circuits for Embedded Systems": "https://www.fhnw.ch/de/studium/module/9435525",
+  // #endregion
+};
