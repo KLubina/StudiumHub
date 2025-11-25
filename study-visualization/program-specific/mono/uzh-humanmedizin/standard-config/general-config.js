2a4fc239dd548e48a38a761e3a7fca5c4732cbd8
Kristian Lubina <kristian.lubina@outlook.com>
2025-11-25 15:23:08 +0100

    add ems average stats in general-config.js

--- study-visualization/program-specific/mono/uzh-humanmedizin/standard-config/general-config.js
+++ study-visualization/program-specific/mono/uzh-humanmedizin/standard-config/general-config.js
@@ -1,3 +1,4 @@
+
 /* ==== GENERAL CONFIGURATION ==== */
 /* Basic information about the study program */
 
@@ -5,19 +6,18 @@ window.StudiengangGeneralConfig = {
     // Program identification
     title: "Bachelor Humanmedizin",
     // Optional HTML subtitle (renders clickable links when provided)
-    subtitleHtml: `<a href="https://www.med.uzh.ch/dam/jcr:ffffffff-c4b3-d46e-0000-000016d79dfa/curriculum_med_2012.pdf" target="_blank" rel="noopener">Curriculum</a> | <a href="https://www.swissuniversities.ch/fileadmin/swissuniversities/Dokumente/Lehre/Medizin/Beispielaufgaben2025.pdf" target="_blank" rel="noopener">NC Beispielaufgaben 2025</a>
-<br><br>
-EMS:
-<br>
-<a href="https://github.com/KLubina/StudiumHub/blob/main/scripts/HumanMed-scripts/EMS/3.1%20EMS%20-%20Teil%20A%20-%20Muster%20zuordnen.pdf" target="_blank" rel="noopener">Teil A - Muster zuordnen.pdf</a><br>
-<a href="https://github.com/KLubina/StudiumHub/blob/main/scripts/HumanMed-scripts/EMS/3.2%20EMS%20-%20Teil%20A%20-%20Medizinisch-naturwissenschaftliches.pdf" target="_blank" rel="noopener">Teil A - Medizinisch-naturwissenschaftliches.pdf</a><br>
-<a href="https://github.com/KLubina/StudiumHub/blob/main/scripts/HumanMed-scripts/EMS/3.3%20EMS%20-%20Teil%20A%20-%20Objekte%20im%20Raum.pdf" target="_blank" rel="noopener">Teil A - Objekte im Raum.pdf</a><br>
-<a href="https://github.com/KLubina/StudiumHub/blob/main/scripts/HumanMed-scripts/EMS/3.4%20EMS%20-%20Teil%20A%20-%20Quantitative%20und%20formale%20Probleme.pdf" target="_blank" rel="noopener">Teil A - Quantitative und formale Probleme.pdf</a><br>
-<a href="https://github.com/KLubina/StudiumHub/blob/main/scripts/HumanMed-scripts/EMS/3.5%20EMS%20-%20Teil%20A%20-%20Fakten%20lernen.pdf" target="_blank" rel="noopener">Teil A - Fakten lernen.pdf</a><br>
-<a href="https://github.com/KLubina/StudiumHub/blob/main/scripts/HumanMed-scripts/EMS/3.6%20EMS%20-%20Teil%20A%20-%20Figuren%20lernen.pdf" target="_blank" rel="noopener">Teil A - Figuren lernen.pdf</a><br>
-<a href="https://github.com/KLubina/StudiumHub/blob/main/scripts/HumanMed-scripts/EMS/4.1%20EMS%20-%20Teil%20B%20-%20Textverstaendnis.pdf" target="_blank" rel="noopener">Teil B - Textverständnis.pdf</a><br>
-<a href="https://github.com/KLubina/StudiumHub/blob/main/scripts/HumanMed-scripts/EMS/4.2%20EMS%20-%20Teil%20B%20-%20Figuren%20lernen.pdf" target="_blank" rel="noopener">Teil B - Figuren lernen.pdf</a><br>
-<a href="https://github.com/KLubina/StudiumHub/blob/main/scripts/HumanMed-scripts/EMS/4.3%20EMS%20-%20Teil%20B%20-%20Fakten%20lernen.pdf" target="_blank" rel="noopener">Teil B - Fakten lernen.pdf</a><br>
-<a href="https://github.com/KLubina/StudiumHub/blob/main/scripts/HumanMed-scripts/EMS/4.4%20EMS%20-%20Teil%20B%20-%20Diagramme%20und%20Tabellen.pdf" target="_blank" rel="noopener">Teil B - Diagramme und Tabellen.pdf</a><br>
-<a href="https://github.com/KLubina/StudiumHub/blob/main/scripts/HumanMed-scripts/EMS/4.5%20EMS%20-%20Teil%20B%20-%20Konzentriertes%20und%20sorgfaeltiges%20Arbeiten.pdf" target="_blank" rel="noopener">Teil B - Konzentriertes und sorgfältiges Arbeiten.pdf</a>`
+    subtitleHtml: `
+<atps://www.med.uzh.ch/dam/jcr:ffffffff-c4b3-d46e-0000-000016d79dfa/curriculum_med_2012.pdf | 
+https://www.swissuniversities.ch/fileadmin/swissuniversities/Dokumente/Lehre/Medizin/Beispielaufgaben2025.pdfNC Beispielaufgaben 2025</a> | 
+<a href="https://emscoaches.ch/numerus-clausus-ems-durchschnittswerte/#:~:text=%E2%80%9EFiguren%20lernen%E2%80%9C%20zeigt%202024%20einen,im%20Numerus%20Clausus%202024%20wider." targetr>
+<a href="https://github.com/KLubina/StudiumHub/blob/main/scripts/HumanMed-scripts/EMS/3.1%20EMS%20-%20Teil%20A%ter%20zuordnen.pdfTeil A - Muster zuordnen.pdf</a><br>
+<a href="https://github.com/KLubina/StudiumHub/blob/main/scripts/HumanMed-scripts/EMS/3.2%20EMS%20-%20Teil%20A%20-%20Medizinisch-naturwissenschaftliches.pdf" target="_-naturwissenschaftliches.pdf</a><br>
+<a href="https://github.com/KLubina/StudiumHub/blob/main/scripts/HumanMed-scripts/EMS/3.3%20EMS%20-%20Teil%20A%ekte%20im%20Raum.pdfTeil A - Objekte im Raum.pdf</a><br>
+<a href="https://github.com/KLubina/StudiumHub/blob/main/scripts/HumanMedEMS/3.4%20EMS%20-%20Teil%20A%20-%20Quantitative%20und%20formale%20Probleme.pdfTeil A - Quantitative und formale Probleme.pdf</a><br>
+<a href="https://github.com/KLubina/StudiumHub/blob/main/scripts/HumanMed-scripts/EMS/3.5%20EMS%20-%20Teil%20Akten%20lernen.pdfTeil A - Fakten lernen.pdf</a><br>
+<atps://github.com/KLubina/StudiumHub/blob/main/scripts/HumanMed-scripts/EMS/3.6%20EMS%20-%20Teil%20A%20-%20Figuren%20lernen.pdf<br>
+<a href="https://github.com/KLubina/StudiumHub/blob/main/scripts/HumanMed-scripts/EMS/4.1%20EMS%20-%20Teil%20extverstaendnis.pdfTeil B - Textverständnis.pdf</a><br>
+<a href="https://github.com/KLubina/StudiumHub/blob/main/scripts/HumanMed-scripts/EMS/4.2%20EMS%20-%20Teil%20B%20-%20Figrnen.pdfTeil B - Figuren lernen.pdf</a><br>
+<a href="https://github.com/KLubina/StudiumHub/blob/main/scripts/HumanMed-scripts/EMS/4.3%20EMS%20-%20Teil%20B%20-%20Fakten%20lernen.pdf" target="_blank" rel="noopeneref="https://github.com/KLubina/StudiumHub/blob/main/scripts/HumanMed-scripts/EMS/4.4%20EMS%2020B%20-%20Diagramme%20und%20Tabellen.pdfTeil B - Diagramme und Tabellen.pdf</a><br>
+<a href="ithub.com/KLubina/StudiumHub/blob/main/scripts/HumanMed-scripts/EMS/4.5%20EMS%20-%20Teil%20B%20-%20Konzentriertes%20und%20sorgfaeltiges%20Arbeiten.pdfTeil B - Konzentriertes und sorgfältiges Arbeiten.pdf</a>`
 };
