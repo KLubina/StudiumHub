44ec68a85d2990c9682ab30b05b8f4df36ff02b5
Kristian Lubina <kristian.lubina@outlook.com>
2026-03-20 00:27:15 +0100

    Add FHNW/OST cards and update FHNW links
    Insert study-visualization cards for OST and FHNW into index.html (removing their previous duplicate entries later in the file). Update study-visualization/program-specific/fhnw-cs/standard-config/general-config.js: replace old subtitle links with current FHNW module table and specialization PDF links and tidy up indentation/formatting.

--- study-visualization/program-specific/fhnw-cs/standard-config/general-config.js
+++ study-visualization/program-specific/fhnw-cs/standard-config/general-config.js
@@ -2,11 +2,15 @@
 /* Basic information about the study program */
 
 window.StudiengangGeneralConfig = {
-    // Program identification
-    title: "BSc Informatik",
+  // Program identification
+  title: "BSc Informatik",
 
-    // Optional HTML subtitle (renders clickable links when provided)
-    subtitleHtml: `<a href="https://www.fhnw.ch/de/studium/informatik/informatik" target="_blank" rel="noopener">Allgemeine Info</a>
-        <br>
-        <a href="https://www.fhnw.ch/de/studium/informatik/informatik/was-beinhaltet-das-studium/media/i-vt.jpg/@@images/image-2520-c0e437c54a97b454b98844a2580b2764.jpeg" target="_blank" rel="noopener">Studienplan (Bild)</a>`
+  // Optional HTML subtitle (renders clickable links when provided)
+  subtitleHtml: `<a href="https://www.fhnw.ch/de/studium/informatik/informatik" target="_blank" rel="noopener">Allgemeine Info</a>
+    <br>
+    <a href="https://www.fhnw.ch/de/informatik/studium/angebot/studiengaenge/informatik/media/modultabelle.pdf/@@display-file/file" target="_blank" rel="noopener">Modultafel Informatik</a>
+    <br>
+    <a href="https://www.fhnw.ch/de/informatik/studium/angebot/studiengaenge/security-platform-devops/modultabelle_spd.pdf/@@display-file/file" target="_blank" rel="noopener">Modultafel SPD</a>
+    <br>
+    <a href="https://www.fhnw.ch/de/informatik/studium/angebot/studiengaenge/informatik/media/schwerpunkte_studiumsrichtungen.pdf/@@display-file/file" target="_blank" rel="noopener">Schwerpunkte Informatik-Fachrichtungen</a>`,
 };
