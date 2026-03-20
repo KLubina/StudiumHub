a04a175f00cdf636a1b4ac9d4336e7e13ccd2e6a
Kristian Lubina <kristian.lubina@outlook.com>
2026-03-20 01:58:17 +0100

    Add Modulübersicht link to FHNW subtitles
    Add a "Modulübersicht" anchor to the subtitleHtml in both fhnw-cs and fhnw-cs-assessment general-config.js files. This inserts a line break and a link to the FHNW module overview page (opens in a new tab with rel="noopener"), providing a direct quick link in the UI.

--- study-visualization/program-specific/fhnw-cs/standard-config/general-config.js
+++ study-visualization/program-specific/fhnw-cs/standard-config/general-config.js
@@ -8,6 +8,8 @@ window.StudiengangGeneralConfig = {
   // Optional HTML subtitle (renders clickable links when provided)
   subtitleHtml: `<a href="https://www.fhnw.ch/de/studium/informatik/informatik" target="_blank" rel="noopener">Allgemeine Info</a>
     <br>
+    <a href="https://www.fhnw.ch/de/informatik/studium/angebot/studiengaenge/informatik/module-uebersicht" target="_blank" rel="noopener">Modulübersicht</a>
+    <br>
     <a href="https://www.fhnw.ch/de/informatik/studium/angebot/studiengaenge/informatik/media/modultabelle.pdf/@@display-file/file" target="_blank" rel="noopener">Modultafel Informatik</a>
     <br>
     <a href="https://www.fhnw.ch/de/informatik/studium/angebot/studiengaenge/security-platform-devops/modultabelle_spd.pdf/@@display-file/file" target="_blank" rel="noopener">Modultafel SPD</a>
