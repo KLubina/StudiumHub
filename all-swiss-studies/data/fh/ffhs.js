5c265e8f761110c6c336a4112d24b19d0ddc04e6
Kristian Lubina <kristian.lubina@outlook.com>
2026-06-01 14:49:26 +0200

    Split uni/FH data into per-institution modules
    Refactor large aggregated data files into per-institution modules under all-swiss-studies/data/{uni,fh}. Added many new university and fachhochschule data files, a loader (all-swiss-studies/data/fh/fachhochschulen.js) to include FH modules, and moved studiengang-mapping.js into all-swiss-studies/. Removed the old aggregated data files (data/fachhochschulen.js and data/universitaeten.js) and updated index.html to reflect the reorganization. This modularization makes the data easier to maintain and load selectively.

---
+++ all-swiss-studies/data/fh/ffhs.js
@@ -0,0 +1,20 @@
+﻿// Auto-generated split from fachhochschulen.js
+(function (global) {
+  global.AlleFHStudiengaengeFFHS = {
+    name: "FFHS",
+    website: "https://www.ffhs.ch/de/bachelor",
+    kategorien: [
+      {
+        name: "1. Ingenieurwissenschaften",
+        unterkategorien: [
+          {
+            name: "Informatik",
+            studiengaenge: [
+              { name: "Informatik", grad: "Bachelor of Science" },
+            ],
+          },
+        ],
+      },
+    ],
+  };
+})(window);
---
+++ all-swiss-studies/data/uni/fernuniversitaet-in-hagen.js
@@ -0,0 +1,20 @@
+﻿// Auto-generated split from universitaeten.js
+(function (global) {
+  global.AlleSchweizerStudiengaengeFernUniversitaetInHagen = {
+        name: "FernUniversität in Hagen",
+        website:
+          "https://www.fernuni-hagen.de/mi/studium/bsc_informatik/studium.shtml",
+        kategorien: [
+          {
+            name: "1. Ingenieurwissenschaften",
+            unterkategorien: [
+              {
+                name: "Informatik",
+                studiengaenge: [{ name: "Informatik", ects: "180 ECTS" }],
+              },
+            ],
+          },
+        ],
+      };
+})(window);
+
