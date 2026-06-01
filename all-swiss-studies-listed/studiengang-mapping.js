30ff9ed204684f58c04cad8d7a83a411fecc2f50
Kristian Lubina <kristian.lubina@outlook.com>
2026-06-01 15:54:33 +0200

    only use studiengang-mappins.js

--- all-swiss-studies-listed/studiengang-mapping.js
+++ all-swiss-studies-listed/studiengang-mapping.js
@@ -1,6 +1,15 @@
+/**
+ * Combined Study Program Mapping & Loader for Swiss Studies
+ * Includes:
+ * 1. StudiengangMapping (studiengang-mapping.js)
+ * 2. Study Program Loader (studiengaenge-loader.js)
+ */
+
+// ==========================================
+// PART 1: Studiengang-Mapping
+// ==========================================
 // Maps study program names from alle-studiengaenge to their visualization keys
 // Only includes programs that have visualizations implemented
-
 (function (global) {
   const StudiengangMapping = {
     // ETH Zürich
@@ -375,4 +384,68 @@
   global.StudiengangMapping = StudiengangMapping;
   // New English alias for internal use
   global.studyProgramMapping = StudiengangMapping;
+})(window);
+
+// ==========================================
+// PART 2: Study Program Loader
+// ==========================================
+// Single entrypoint for all Swiss study program data.
+(function (global) {
+  const baseUrl = new URL(".", document.currentScript.src);
+
+  const files = [
+    "uni/universitaet-basel.js",
+    "uni/universitaet-luzern.js",
+    "uni/universitaet-st-gallen.js",
+    "uni/universitaet-bern.js",
+    "uni/universitaet-freiburg.js",
+    "uni/eth-zuerich.js",
+    "uni/universitaet-zuerich.js",
+    "uni/fernuniversitaet-in-hagen.js",
+    "fh/berner-fachhochschule.js",
+    "fh/fh-graubuenden.js",
+    "fh/fhnw.js",
+    "fh/ostschweizer-fachhochschule.js",
+    "fh/zhaw.js",
+    "fh/zhdk.js",
+    "fh/hslu.js",
+    "fh/ffhs.js",
+  ];
+
+  const scriptTags = files
+    .map(
+      (file) => `<script src="${new URL(file, baseUrl).toString()}"><\/script>`,
+    )
+    .join("");
+
+  const initScript = `
+    <script>
+      window.AlleSchweizerStudiengaenge = {
+        universitaeten: [
+          window.AlleSchweizerStudiengaengeUniversitaetBasel,
+          window.AlleSchweizerStudiengaengeUniversitaetLuzern,
+          window.AlleSchweizerStudiengaengeUniversitaetStGallen,
+          window.AlleSchweizerStudiengaengeUniversitaetBern,
+          window.AlleSchweizerStudiengaengeUniversitaetFreiburg,
+          window.AlleSchweizerStudiengaengeETHZuerich,
+          window.AlleSchweizerStudiengaengeUniversitaetZuerich,
+          window.AlleSchweizerStudiengaengeFernUniversitaetInHagen,
+        ].filter(Boolean),
+      };
+
+      window.AlleFHStudiengaenge = {
+        fachhochschulen: [
+          window.AlleFHStudiengaengeBernerFachhochschule,
+          window.AlleFHStudiengaengeFHGraubuenden,
+          window.AlleFHStudiengaengeFHNW,
+          window.AlleFHStudiengaengeOstschweizerFachhochschule,
+          window.AlleFHStudiengaengeZHAW,
+          window.AlleFHStudiengaengeZHdK,
+          window.AlleFHStudiengaengeHSLU,
+          window.AlleFHStudiengaengeFFHS,
+        ].filter(Boolean),
+      };
+    <\/script>`;
+
+  document.write(scriptTags + initScript);
 })(window);
