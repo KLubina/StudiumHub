6c25b9f76871b0145a8ed1de246e7c3d49b33c06
Kristian Lubina <kristian.lubina@outlook.com>
2026-06-17 22:03:20 +0200

    add BIT to FHNW Wirtschaftsinformatik

--- study-visualization/program-specific/fhnw-wirtschaftsinformatik/standard-config/general-config.js
+++ study-visualization/program-specific/fhnw-wirtschaftsinformatik/standard-config/general-config.js
@@ -1,8 +1,14 @@
 /* ==== GENERAL CONFIGURATION ==== */
 /* Basic information about the study program */
-
 window.StudiengangGeneralConfig = {
+  // Program identification
   title: "Bachelor Wirtschaftsinformatik (FHNW)",
-  subtitleHtml:
-    'Weitere Informationen: <a href="https://www.fhnw.ch/de/wirtschaft/studium/angebot/studiengaenge/bachelor-in-wirtschaftsinformatik" target="_blank" rel="noopener">FHNW Studiengangseite</a> | <a href="https://www.fhnw.ch/++api++/de/wirtschaft/studium/angebot/studiengaenge/media/web_wi_cur25_moduluebersicht_tz.pdf/@@inline-file/file" target="_blank" rel="noopener">Modulübersicht (PDF)</a>',
+  // Optional HTML subtitle (renders clickable links when provided)
+  subtitleHtml: `<a href="https://www.fhnw.ch/de/wirtschaft/studium/angebot/studiengaenge/bachelor-in-wirtschaftsinformatik" target="_blank" rel="noopener">FHNW Studiengangseite</a>
+        <br>
+        <a href="https://www.fhnw.ch/++api++/de/wirtschaft/studium/angebot/studiengaenge/media/web_wi_cur25_moduluebersicht_tz.pdf/@@inline-file/file" target="_blank" rel="noopener">Modulübersicht (PDF)</a>
+        <br>
+        <a href="https://www.fhnw.ch/de/wirtschaft/studium/angebot/studiengaenge/bachelor-in-business-information-technology" target="_blank" rel="noopener">BIT Studiengangseite</a>
+        <br>
+        <a href="https://www.fhnw.ch/++api++/de/wirtschaft/studium/angebot/studiengaenge/media/web_bit_cur25_moduluebersicht_tz.pdf/@@inline-file/file" target="_blank" rel="noopener">BIT Modulübersicht (PDF)</a>`,
 };
