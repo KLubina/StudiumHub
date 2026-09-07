84143b6f9f44abfaca8bf68c7863400eefa61022
Kristian Lubina <kristian.lubina@outlook.com>
2026-09-07 21:44:57 +0200

    Clean up notes and add ZHAW module details
    This commit adds detailed mathematics module information for the ZHAW Biotechnologie and Chemie study programs, including learning content and module links. It also removes obsolete personal study notes for Betriebsökonomie and ITET-ETH that are no longer needed.

--- study-visualization/program-specific/zhaw-biotechnologie/data/basic-modules-details.js
+++ study-visualization/program-specific/zhaw-biotechnologie/data/basic-modules-details.js
@@ -1,1 +1,21 @@
-﻿window.StudiengangModuleDetails = {};
+﻿window.StudiengangModuleDetails = {
+  "Mathematik 1": {
+    inhalt:
+      "• Grundlagen (Repetition)\n" +
+      "• Folgen, Reihen & Grenzwerte\n" +
+      "• Elementare Funktionen, deren Kombinationen & Anwendung\n" +
+      "• Bedeutung und Regeln der Ableitung\n" +
+      "• Kurvendiskussion & Näherungsverfahren\n" +
+      "• Bedeutung der Integration, elementare Integrationstechniken",
+    link: "https://eventoweb.zhaw.ch/Evt_Pages/Brn_ModulDetailAZ.aspx?node=c594e3e5-cd9a-4204-9a61-de1e43ccb7b0&IDAnlass=1696292",
+  },
+  "Mathematik 2": {
+    inhalt:
+      "• Das Integral als Bilanzgrösse\n" +
+      "• Grundlagen der linearen Algebra (Vektoren & Matrizen)\n" +
+      "• Gewöhnliche Differenzialgleichungen\n" +
+      "• Funktionen von mehreren Variablen\n" +
+      "• Fourier-Entwicklung und Fourier-Transformation",
+    link: "https://eventoweb.zhaw.ch/Evt_Pages/Brn_ModulDetailAZ.aspx?node=c594e3e5-cd9a-4204-9a61-de1e43ccb7b0&IDAnlass=1696381",
+  },
+};
--- study-visualization/program-specific/zhaw-chemie/data/basic-modules-details.js
+++ study-visualization/program-specific/zhaw-chemie/data/basic-modules-details.js
@@ -1,1 +1,20 @@
-﻿window.StudiengangModuleDetails = {};
+﻿window.StudiengangModuleDetails = {
+  "Mathematik 1": {
+    inhalt:
+      "• Folgen und Grenzwert\n" +
+      "• Definition, Eigenschaften und Darstellung von elementaren Funktionen\n" +
+      "• Konzept und Technik der Differentialrechnung\n" +
+      "• Kurvendiskussion\n" +
+      "• Konzept der Integralrechnung, elementare Integrationstechniken\n" +
+      "• Extremalwertprobleme",
+    link: "https://eventoweb.zhaw.ch/Evt_Pages/Brn_ModulDetailAZ.aspx?node=c594e3e5-cd9a-4204-9a61-de1e43ccb7b0&IDAnlass=1696422",
+  },
+  "Mathematik 2": {
+    inhalt:
+      "• Trigonometrische Funktionen im Kontext der Differential- und Integralrechnung\n" +
+      "• Konzept und Lösungsverfahren von gewöhnlichen Differentialgleichungen mit Anwendungen\n" +
+      "• Grundlagen der Matrizenrechnung\n" +
+      "• Funktionen mehrerer Veränderlicher",
+    link: "https://eventoweb.zhaw.ch/Evt_Pages/Brn_ModulDetailAZ.aspx?node=c594e3e5-cd9a-4204-9a61-de1e43ccb7b0&IDAnlass=1696483",
+  },
+};
